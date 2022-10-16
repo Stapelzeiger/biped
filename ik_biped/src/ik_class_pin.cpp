44622d4cc990591bbbea6e217a79f688b00c8b3f
Sorina <lupusorina@yahoo.com>
2022-10-16 16:25:56 -0700

    uncomment ik part

--- ik_biped/src/ik_class_pin.cpp
+++ ik_biped/src/ik_class_pin.cpp
@@ -15,118 +15,113 @@ void IKRobot::build_model(const std::string urdf_xml_string)
     // std::cout << "model frame:" << idx_frame << std::endl;
 }
 
-//     int get_size_q()
-//     {
-//         return model.nq;
-//     }
+int IKRobot::get_size_q()
+{
+    return model.nq;
+}
 
-//     int get_size_q_dot()
-//     {
-//         return model.nv;
-//     }
+int IKRobot::get_size_q_dot()
+{
+    return model.nv;
+}
 
-//     Eigen::VectorXd get_desired_q(Eigen::VectorXd q, Eigen::Vector3d pos_foot_des, double yaw_angle, std::string joint_name)
-//     {
-//         auto start = high_resolution_clock::now();
+Eigen::VectorXd IKRobot::get_desired_q(Eigen::VectorXd q, Eigen::Vector3d pos_foot_des, double yaw_angle, std::string joint_name)
+{
 
-//         pinocchio::Data data(model);
+    pinocchio::Data data(model);
 
-//         int joint_id;
-//         joint_id = model.getJointId(joint_name);
+    int joint_id;
+    joint_id = model.getJointId(joint_name);
 
-//         int put_to_zero_joints[5];
+    int put_to_zero_joints[5];
 
-//         if (joint_name == "FL_ANKLE")
-//         {
-//             put_to_zero_joints[0] = 11;
-//             put_to_zero_joints[1] = 12;
-//             put_to_zero_joints[2] = 13;
-//             put_to_zero_joints[3] = 14;
-//             put_to_zero_joints[4] = 15;
-//         }
-//         if (joint_name == "FR_ANKLE")
-//         {
-//             put_to_zero_joints[0] = 6;
-//             put_to_zero_joints[1] = 7;
-//             put_to_zero_joints[2] = 8;
-//             put_to_zero_joints[3] = 9;
-//             put_to_zero_joints[4] = 10;
-//         }
+    if (joint_name == "FL_ANKLE")
+    {
+        put_to_zero_joints[0] = 11;
+        put_to_zero_joints[1] = 12;
+        put_to_zero_joints[2] = 13;
+        put_to_zero_joints[3] = 14;
+        put_to_zero_joints[4] = 15;
+    }
+    if (joint_name == "FR_ANKLE")
+    {
+        put_to_zero_joints[0] = 6;
+        put_to_zero_joints[1] = 7;
+        put_to_zero_joints[2] = 8;
+        put_to_zero_joints[3] = 9;
+        put_to_zero_joints[4] = 10;
+    }
 
-//         const pinocchio::SE3 oMdes(Eigen::Matrix3d::Identity(), pos_foot_des);
+    const pinocchio::SE3 oMdes(Eigen::Matrix3d::Identity(), pos_foot_des);
 
-//         pinocchio::Data::Matrix6x J(6, model.nv);
-//         Eigen::Matrix<double, 5, Eigen::Dynamic> J_truncated(5, model.nv);
+    pinocchio::Data::Matrix6x J(6, model.nv);
+    Eigen::Matrix<double, 5, Eigen::Dynamic> J_truncated(5, model.nv);
 
-//         J.setZero();
-//         J_truncated.setZero();
+    J.setZero();
+    J_truncated.setZero();
 
-//         Eigen::Matrix<double, 5, 1> err;
-//         Eigen::Matrix<double, 3, 1> err_truncated;
+    Eigen::Matrix<double, 5, 1> err;
+    Eigen::Matrix<double, 3, 1> err_truncated;
 
-//         Eigen::VectorXd v(model.nv);
-//         Eigen::Vector3d vector_x_in_foot_frame;
+    Eigen::VectorXd v(model.nv);
+    Eigen::Vector3d vector_x_in_foot_frame;
 
-//         double yaw_error;
-//         double pitch_error;
+    double yaw_error;
+    double pitch_error;
 
-//         bool success = false;
-//         int i;
-//         for (i = 0;; i++)
-//         {
-//             pinocchio::forwardKinematics(model, data, q);
-//             const pinocchio::SE3 dMi = oMdes.actInv(data.oMi[joint_id]);
+    int i;
+    for (i = 0;; i++)
+    {
+        pinocchio::forwardKinematics(model, data, q);
+        const pinocchio::SE3 dMi = oMdes.actInv(data.oMi[joint_id]);
 
-//             vector_x_in_foot_frame = data.oMi[joint_id].rotation().transpose() * Eigen::Vector3d(cos(yaw_angle), sin(yaw_angle), 0);
+        vector_x_in_foot_frame = data.oMi[joint_id].rotation().transpose() * Eigen::Vector3d(cos(yaw_angle), sin(yaw_angle), 0);
 
-//             pitch_error = vector_x_in_foot_frame[1];
-//             yaw_error = vector_x_in_foot_frame[2];
+        pitch_error = vector_x_in_foot_frame[1];
+        yaw_error = vector_x_in_foot_frame[2];
 
-//             // err = pinocchio::log6(dMi).toVector();
-//             // err_truncated[0] = err[0]; err_truncated[1] = err[1]; err_truncated[2] = err[2];
-//             err_truncated = -dMi.inverse().translation();
-//             err.row(0) = err_truncated.row(0);
-//             err.row(1) = err_truncated.row(1);
-//             err.row(2) = err_truncated.row(2);
-//             err[3] = -pitch_error;
-//             err[4] = -yaw_error;
+        // err = pinocchio::log6(dMi).toVector();
+        // err_truncated[0] = err[0]; err_truncated[1] = err[1]; err_truncated[2] = err[2];
+        err_truncated = -dMi.inverse().translation();
+        err.row(0) = err_truncated.row(0);
+        err.row(1) = err_truncated.row(1);
+        err.row(2) = err_truncated.row(2);
+        err[3] = -pitch_error;
+        err[4] = -yaw_error;
 
-//             if (err.norm() < eps)
-//             {
-//                 success = true;
-//                 break;
-//             }
+        if (err.norm() < eps)
+        {
+            break;
+        }
 
-//             if (i >= IT_MAX)
-//             {
-//                 success = false;
-//                 break;
-//             }
-//             auto start_matrix_stuff = high_resolution_clock::now();
+        if (i >= IT_MAX)
+        {
+            break;
+        }
 
-//             pinocchio::computeJointJacobian(model, data, q, joint_id, J);
-//             J_truncated.block(0, 0, 3, model.nv) = J.block(0, 0, 3, model.nv);
-//             J_truncated.row(3) = 1 * J.row(5);
-//             J_truncated.row(4) = -1 * J.row(4);
+        pinocchio::computeJointJacobian(model, data, q, joint_id, J);
+        J_truncated.block(0, 0, 3, model.nv) = J.block(0, 0, 3, model.nv);
+        J_truncated.row(3) = 1 * J.row(5);
+        J_truncated.row(4) = -1 * J.row(4);
 
-//             J_truncated.block(0, 0, 5, 5) = Eigen::MatrixXd::Zero(5, 5);
+        J_truncated.block(0, 0, 5, 5) = Eigen::MatrixXd::Zero(5, 5);
 
-//             J_truncated.col(put_to_zero_joints[0]).setZero();
-//             J_truncated.col(put_to_zero_joints[1]).setZero();
-//             J_truncated.col(put_to_zero_joints[2]).setZero();
-//             J_truncated.col(put_to_zero_joints[3]).setZero();
-//             J_truncated.col(put_to_zero_joints[4]).setZero();
+        J_truncated.col(put_to_zero_joints[0]).setZero();
+        J_truncated.col(put_to_zero_joints[1]).setZero();
+        J_truncated.col(put_to_zero_joints[2]).setZero();
+        J_truncated.col(put_to_zero_joints[3]).setZero();
+        J_truncated.col(put_to_zero_joints[4]).setZero();
 
-//             Eigen::Matrix<double, 5, Eigen::Dynamic> JJt;
-//             JJt.noalias() = J_truncated * J_truncated.transpose();
-//             JJt.diagonal().array() += damp;
-//             v.noalias() = -J_truncated.transpose() * JJt.ldlt().solve(err);
+        Eigen::Matrix<double, 5, Eigen::Dynamic> JJt;
+        JJt.noalias() = J_truncated * J_truncated.transpose();
+        JJt.diagonal().array() += damp;
+        v.noalias() = -J_truncated.transpose() * JJt.ldlt().solve(err);
 
-//             q = pinocchio::integrate(model, q, v * DT);
-//         }
+        q = pinocchio::integrate(model, q, v * DT);
+    }
 
-//         return q;
-//     }
+    return q;
+}
 
 //     Eigen::MatrixXd get_J(Eigen::VectorXd q, std::string joint_name)
 //     {
