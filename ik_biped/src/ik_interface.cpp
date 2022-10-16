b8b3d7283d0b8462487ade2c4e5be84773a35032
Sorina <lupusorina@yahoo.com>
2022-10-16 16:27:33 -0700

    pub and sub for the ik node

--- ik_biped/src/ik_interface.cpp
+++ ik_biped/src/ik_interface.cpp
@@ -7,32 +7,60 @@
 
 using namespace std::placeholders;
 
-class MinimalSubscriber : public rclcpp::Node
+class IKNode : public rclcpp::Node
 {
+
 public:
-    MinimalSubscriber()
-        : Node("ik_subscriber_urdf")
+    IKNode() : Node("ik_node")
     {
-        subscription_ = this->create_subscription<std_msgs::msg::String>(
-            "/robot_description", 10, std::bind(&MinimalSubscriber::topic_callback, this, _1));
+        robot_desc_sub_ = this->create_subscription<std_msgs::msg::String>(
+            "/robot_description", rclcpp::QoS(rclcpp::KeepLast(1)).transient_local(), std::bind(&IKNode::robot_desc_cb, this, _1));
+
+        // subscribe to current q
+        // subscribe to foot desired
+
+        robot_joints_pub_ = this->create_publisher<sensor_msgs::msg::JointState>("joint_states", 10);
+        timer_ = this->create_wall_timer(
+            500ms, std::bind(&IKNode::timer_pub_robot_joints, this));
     }
 
 private:
-    void topic_callback(const std_msgs::msg::String::SharedPtr msg) const
+    void timer_pub_robot_joints()
     {
-        IKRobot ik_robot;
-        ik_robot.build_model(msg->data.c_str());
-        RCLCPP_INFO(this->get_logger(), "I heard: '%s'", msg->data.c_str());
+
+        Eigen::VectorXd q_current(17, 1);
+        q_current << 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0;
+        Eigen::Vector3d pos_foot_des(3, 1);
+        std::cout << q_current << std::endl;
+        pos_foot_des << -0.2, 0.0, 0.0;
+        double yaw_angle_des = 0.0;
+        std::string joint_name = "FL_ANKLE";
+        Eigen::VectorXd q_des;
+        q_des = robot_.get_desired_q(q_current, pos_foot_des, yaw_angle_des, joint_name);
+        std::cout << q_des << std::endl;
+
+        auto message = sensor_msgs::msg::JointState;
+
+        robot_joints_pub_->publish(message);
     }
-    rclcpp::Subscription<std_msgs::msg::String>::SharedPtr subscription_;
+
+    void robot_desc_cb(const std_msgs::msg::String::SharedPtr msg)
+    {
+
+        robot_.build_model(msg->data.c_str());
+    }
+    rclcpp::TimerBase::SharedPtr timer_;
+    rclcpp::Publisher<std_msgs::msg::String>::SharedPtr robot_joints_pub_;
+    size_t count_;
+
+    rclcpp::Subscription<std_msgs::msg::String>::SharedPtr robot_desc_sub_;
+    IKRobot robot_;
 };
 
 int main(int argc, char *argv[])
 {
     rclcpp::init(argc, argv);
-    const std::string urdf_filename = std::string("../../biped_robot_description/urdf/custom_robot.urdf");
-
-    rclcpp::spin(std::make_shared<MinimalSubscriber>());
+    rclcpp::spin(std::make_shared<IKNode>());
     rclcpp::shutdown();
     return 0;
 }
