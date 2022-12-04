e2b49e334e5fbc88e8caa56580c167d0738d60c4
Sorina <lupusorina@yahoo.com>
2022-12-03 21:27:10 -0800

    change authorname and fix the launch directory

--- sim_mujoco/setup.py
+++ sim_mujoco/setup.py
@@ -1,4 +1,6 @@
 from setuptools import setup
+from glob import glob
+import os
 
 package_name = 'sim_mujoco'
 
@@ -10,11 +12,12 @@ setup(
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
+        (os.path.join('share', package_name), glob('launch/*launch.[pxy][yma]*'))
     ],
     install_requires=['setuptools'],
     zip_safe=True,
-    maintainer='Patrick Spieler',
-    maintainer_email='stapelzeiger@gmail.com',
+    maintainer='Sorina Lupu',
+    maintainer_email='lupusorina@yahoo.com',
     description='TODO: Package description',
     license='TODO: License declaration',
     tests_require=['pytest'],
@@ -24,3 +27,4 @@ setup(
         ],
     },
 )
+
