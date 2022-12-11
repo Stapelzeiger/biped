ad22ab2d582124d3c90fa33b3d5d4cbf15fb6b92
Patrick Spieler <stapelzeiger@gmail.com>
2022-12-11 01:50:20 +0000

    joystick script publish at high rate

--- moteus_drv/src/joy.py
+++ moteus_drv/src/joy.py
@@ -1,5 +1,6 @@
 import rclpy
 from rclpy.node import Node
+import threading
 
 from trajectory_msgs.msg import JointTrajectory, JointTrajectoryPoint
 from sensor_msgs.msg import Joy
@@ -7,23 +8,34 @@ from sensor_msgs.msg import Joy
 class JoyNode(Node):
     def __init__(self):
         super().__init__('JoyMotorControl')
-        self.pub = self.create_publisher(JointTrajectory, "/moteus/joint_traj", 1)
+        self.lock = threading.Lock()
+        self.pub = self.create_publisher(JointTrajectory, "/joint_trajectory", 1)
         self.subscription = self.create_subscription(
             Joy,
             '/joy',
             self.joy_cb,
             1)
+        self.timer = self.create_timer(0.01, self.timer_cb)
+        self.joy = None
 
-    def joy_cb(self, msg):
+    def timer_cb(self):
+        with self.lock:
+            joy = self.joy
+        if joy is None:
+            return
         ctrl = JointTrajectory()
+        ctrl.header.stamp = self.get_clock().now().to_msg()
         ctrl.joint_names = ['FR_YAW']
         ctrl.points = [JointTrajectoryPoint()]
-        ctrl.points[0].positions.append(msg.axes[5]*0.1)
+        ctrl.points[0].positions.append(joy.axes[5]*3.15*0.5*0.2)
         ctrl.points[0].velocities.append(0)
         ctrl.points[0].effort.append(0)
-        print(ctrl)
         self.pub.publish(ctrl)
-        
+
+    def joy_cb(self, msg):
+        with self.lock:
+            self.joy = msg
+
 
 
 def main(args=None):
