e161651657e8f4689c19f715117256641b4ca9b1
Patrick Spieler <stapelzeiger@gmail.com>
2022-12-11 01:49:42 +0000

    fix memory error for timed out command

--- moteus_drv/src/moteus.cpp
+++ moteus_drv/src/moteus.cpp
@@ -123,21 +123,23 @@ private:
                 // check for timeout and index in trajectory
                 if ((now - joint_traj_[joint_idx].header.stamp).seconds() > joint_command_timeout_) {
                     joint_traj_[joint_idx].points.clear();
-                }
-                const double revolutions = 1/(2*M_PI);
-                size_t traj_idx = 0; // TODO: handle multiple traj points
-                moteus_command_buf_[joint_idx].mode = moteus::Mode::kPosition;
-                if (joint_traj_[joint_idx].points[traj_idx].positions.size() == 1) {
-                    double p = joint_traj_[joint_idx].points[traj_idx].positions[0];
-                    moteus_command_buf_[joint_idx].position.position = revolutions * (p * joint_signs_[joint_idx] + joint_offsets_[joint_idx]);
-                }
-                if (joint_traj_[joint_idx].points[traj_idx].velocities.size() == 1) {
-                    double v = joint_traj_[joint_idx].points[traj_idx].velocities[0];
-                    moteus_command_buf_[joint_idx].position.velocity = revolutions * v * joint_signs_[joint_idx];
-                }
-                if (joint_traj_[joint_idx].points[traj_idx].effort.size() == 1) {
-                    double t = joint_traj_[joint_idx].points[traj_idx].effort[0];
-                    moteus_command_buf_[joint_idx].position.feedforward_torque = t * joint_signs_[joint_idx];
+                    RCLCPP_WARN_STREAM(this->get_logger(), "Joint " << joint_names_[joint_idx] << " timed out");
+                } else {
+                    const double revolutions = 1/(2*M_PI);
+                    size_t traj_idx = 0; // TODO: handle multiple traj points
+                    moteus_command_buf_[joint_idx].mode = moteus::Mode::kPosition;
+                    if (joint_traj_[joint_idx].points[traj_idx].positions.size() == 1) {
+                        double p = joint_traj_[joint_idx].points[traj_idx].positions[0];
+                        moteus_command_buf_[joint_idx].position.position = revolutions * (p * joint_signs_[joint_idx] + joint_offsets_[joint_idx]);
+                    }
+                    if (joint_traj_[joint_idx].points[traj_idx].velocities.size() == 1) {
+                        double v = joint_traj_[joint_idx].points[traj_idx].velocities[0];
+                        moteus_command_buf_[joint_idx].position.velocity = revolutions * v * joint_signs_[joint_idx];
+                    }
+                    if (joint_traj_[joint_idx].points[traj_idx].effort.size() == 1) {
+                        double t = joint_traj_[joint_idx].points[traj_idx].effort[0];
+                        moteus_command_buf_[joint_idx].position.feedforward_torque = t * joint_signs_[joint_idx];
+                    }
                 }
             }
         }
