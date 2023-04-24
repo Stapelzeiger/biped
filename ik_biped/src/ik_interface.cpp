6b0d2476de4213d39c09038f064cdaa1aab19519
Sorina <lupusorina@yahoo.com>
2023-04-23 18:18:27 -0700

    make an input marker and change pub name

--- ik_biped/src/ik_interface.cpp
+++ ik_biped/src/ik_interface.cpp
@@ -25,8 +25,8 @@ public:
         robot_desc_sub_ = this->create_subscription<std_msgs::msg::String>(
             "/robot_description", rclcpp::QoS(rclcpp::KeepLast(1)).transient_local(), std::bind(&IKNode::robot_desc_cb, this, _1));
 
-        foot_desired_sub_ = this->create_subscription<trajectory_msgs::msg::MultiDOFJointTrajectory>(
-            "foot_positions", 10, std::bind(&IKNode::foot_desired_cb, this, _1));
+        body_desired_sub_ = this->create_subscription<trajectory_msgs::msg::MultiDOFJointTrajectory>(
+            "body_trajectories", 10, std::bind(&IKNode::body_desired_cb, this, _1));
 
         contact_right_sub_ = this->create_subscription<biped_bringup::msg::StampedBool>(
             "~/contact_foot_right", 10, std::bind(&IKNode::contact_right_cb, this, _1));
@@ -44,7 +44,7 @@ private:
         double dt = (now - rclcpp::Time(msg->header.stamp)).seconds();
         if (dt > 0.1)
         {
-            RCLCPP_WARN(this->get_logger(), "contact right footsensor data is too old: %f", dt);
+            RCLCPP_WARN(this->get_logger(), "contact right foot sensor data is too old: %f", dt);
         }
 
         foot_right_contact_ = msg->data;
@@ -56,13 +56,13 @@ private:
         double dt = (now - rclcpp::Time(msg->header.stamp)).seconds();
         if (dt > 0.1)
         {
-            RCLCPP_WARN(this->get_logger(), "contact left footsensor data is too old: %f", dt);
+            RCLCPP_WARN(this->get_logger(), "contact left foot sensor data is too old: %f", dt);
         }
 
         foot_left_contact_ = msg->data;
     }
 
-    void foot_desired_cb(trajectory_msgs::msg::MultiDOFJointTrajectory::SharedPtr msg)
+    void body_desired_cb(trajectory_msgs::msg::MultiDOFJointTrajectory::SharedPtr msg)
     {
         if (!robot_.has_model()) {
             RCLCPP_ERROR_SKIPFIRST_THROTTLE(this->get_logger(), *this->get_clock(), 1000 /* [ms] */, "No robot model loaded");
@@ -86,14 +86,12 @@ private:
             }
 
             std::vector<IKRobot::BodyState> bodies;
-            // if base_link is missing, create it at zero
+            // check if baselink is missing
             if (std::find(msg->joint_names.begin(), msg->joint_names.end(), "base_link") == msg->joint_names.end()) {
-                if (msg->header.frame_id != "base_link") {
-                    RCLCPP_ERROR(this->get_logger(), "base_link missing in MultiDofJoints and frame_id is not base_link");
-                    return;
-                }
-                bodies.push_back(IKRobot::BodyState("base_link", Eigen::Vector3d::Zero(), Eigen::Quaterniond::Identity()));
+                RCLCPP_ERROR(this->get_logger(), "base_link missing in MultiDofJoints");
+                return;
             }
+
             for (size_t i = 0; i < msg->joint_names.size(); i++) {
                 const auto &name = msg->joint_names[i];
                 const auto &pos = Eigen::Vector3d(pt.transforms[i].translation.x, pt.transforms[i].translation.y, pt.transforms[i].translation.z);
@@ -166,7 +164,7 @@ private:
 
             if (markers_pub_->get_subscription_count() > 0) {
                 visualization_msgs::msg::MarkerArray marker_array_msg;
-                marker_array_msg.markers.resize(body_positions_solution.size() * 2);
+                marker_array_msg.markers.resize(body_positions_solution.size() * 3);
                 assert(body_positions_solution.size() == bodies.size());
                 assert(body_positions_solution.size() == pt.transforms.size());
                 for (unsigned i = 0; i < body_positions_solution.size(); i++)
@@ -204,6 +202,25 @@ private:
                     marker_array_msg.markers[i + body_positions_solution.size()].color.r = 1.0; // red blob
                     marker_array_msg.markers[i + body_positions_solution.size()].color.g = 0.0;
                     marker_array_msg.markers[i + body_positions_solution.size()].color.b = 0.0;
+
+
+                    marker_array_msg.markers[i + 2*body_positions_solution.size()].header = msg->header;
+                    marker_array_msg.markers[i + 2*body_positions_solution.size()].ns = bodies[i].name + "_input";
+                    marker_array_msg.markers[i + 2*body_positions_solution.size()].id = pt_idx;
+                    marker_array_msg.markers[i + 2*body_positions_solution.size()].type = visualization_msgs::msg::Marker::SPHERE;
+                    marker_array_msg.markers[i + 2*body_positions_solution.size()].action = visualization_msgs::msg::Marker::ADD;
+                    marker_array_msg.markers[i + 2*body_positions_solution.size()].pose.position.x = bodies[i].position[0];
+                    marker_array_msg.markers[i + 2*body_positions_solution.size()].pose.position.y = bodies[i].position[1];
+                    marker_array_msg.markers[i + 2*body_positions_solution.size()].pose.position.z = bodies[i].position[2];
+                    marker_array_msg.markers[i + 2*body_positions_solution.size()].pose.orientation.w = 1.0;
+                    marker_array_msg.markers[i + 2*body_positions_solution.size()].scale.x = 0.05;
+                    marker_array_msg.markers[i + 2*body_positions_solution.size()].scale.y = 0.05;
+                    marker_array_msg.markers[i + 2*body_positions_solution.size()].scale.z = 0.05;
+                    marker_array_msg.markers[i + 2*body_positions_solution.size()].color.a = 1.0;
+                    marker_array_msg.markers[i + 2*body_positions_solution.size()].color.r = 0.0;
+                    marker_array_msg.markers[i + 2*body_positions_solution.size()].color.g = 0.0;
+                    marker_array_msg.markers[i + 2*body_positions_solution.size()].color.b = 1.0; // blue blob
+
                 }
                 markers_pub_->publish(marker_array_msg);
             }
@@ -222,7 +239,7 @@ private:
     rclcpp::Publisher<trajectory_msgs::msg::JointTrajectory>::SharedPtr robot_joints_pub_;
     rclcpp::Publisher<visualization_msgs::msg::MarkerArray>::SharedPtr markers_pub_;
     rclcpp::Subscription<std_msgs::msg::String>::SharedPtr robot_desc_sub_;
-    rclcpp::Subscription<trajectory_msgs::msg::MultiDOFJointTrajectory>::SharedPtr foot_desired_sub_;
+    rclcpp::Subscription<trajectory_msgs::msg::MultiDOFJointTrajectory>::SharedPtr body_desired_sub_;
     rclcpp::Subscription<biped_bringup::msg::StampedBool>::SharedPtr contact_right_sub_;
     rclcpp::Subscription<biped_bringup::msg::StampedBool>::SharedPtr contact_left_sub_;
 
