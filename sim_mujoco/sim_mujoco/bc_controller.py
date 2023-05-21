ec529e13f7b2a8f9a87678d4ed92303e7a37613b
Sorina <lupusorina@yahoo.com>
2023-05-20 18:13:46 -0700

    controller with beh. cloning

---
+++ sim_mujoco/sim_mujoco/bc_controller.py
@@ -0,0 +1,29 @@
+import numpy as np
+import torch
+
+from bc import BC_Agent
+
+class PolicyBC:
+    def __init__(self, state_size, action_size, num_input_states):
+        self.state_size = state_size
+        self.action_size = action_size
+        self.folder = '/home/sorina/Documents/code/biped_hardware/ros2_ws/src/biped/sim_mujoco/imit_learning_setup/behavioral_cloning/' # todo fix this so it's not hardcoded
+        self.policy_name = 'bc_policy_4_500e'
+
+        num_input_states = num_input_states # Number of states to include in input
+        policy_arch = [
+            {'Layer': 'Linear', 'Input': self.state_size * num_input_states, 'Output': 256},
+            {'Layer': 'ReLU'},
+            {'Layer': 'Linear', 'Input': 256, 'Output': 512},
+            {'Layer': 'ReLU'},
+            {'Layer': 'Linear', 'Input': 512, 'Output': 256},
+            {'Layer': 'ReLU'},
+            {'Layer': 'Linear', 'Input': 256, 'Output': self.action_size}
+        ]
+
+        self.policy = BC_Agent(policy_arch, torch.zeros(self.state_size), torch.zeros(self.state_size), torch.zeros(self.action_size), torch.zeros(self.action_size))
+        self.policy.load_policy(self.folder + f"policies/{self.policy_name}.pt")
+
+    def __call__(self, input_state):
+        policy_actions = self.policy(torch.tensor(input_state, dtype=torch.float32))
+        return policy_actions.detach().numpy()
