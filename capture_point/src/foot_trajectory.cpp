aac481453dc04cfafb2b99ef449dafbc2f067e8d
Sorina <lupusorina@yahoo.com>
2023-06-23 11:07:53 -0700

    make spline a separate fcn

--- capture_point/src/foot_trajectory.cpp
+++ capture_point/src/foot_trajectory.cpp
@@ -1,10 +1,48 @@
 #include "foot_trajectory.h"
 #include <cmath>
+
 using namespace std;
 
-FootTrajectory::FootTrajectory()
+SplineTrajectory::SplineTrajectory(){};
+SplineTrajectory::~SplineTrajectory(){};
+
+Eigen::Vector<double, 4> SplineTrajectory::get_spline_coef(double tf, double qi, double qi_dot, double qf, double qf_dot)
 {
+    Eigen::Vector<double, 4> coeff; // a0 a1 a2 a3
+    coeff(0) = qi;
+    coeff(1) = qi_dot;
+
+    Eigen::MatrixXd mat(2,2);
+    mat(0,0) = pow(tf, 3); mat(0,1) = pow(tf, 2);
+    mat(1,0) = 3*pow(tf, 2); mat(1,1) = 2*tf;
+    Eigen::MatrixXd mat_inv = mat.inverse();
+
+    Eigen::VectorXd vec(2);
+    vec(0) = qf - qi - qi_dot*tf;
+    vec(1) = qf_dot - qi_dot;
+
+    Eigen::VectorXd vec2 = mat_inv * vec;
+    coeff(3) = vec2(0);
+    coeff(2) = vec2(1);
+
+    return coeff;
 }
+
+double SplineTrajectory::get_q(Eigen::Vector<double, 4> coeff, double t)
+{
+    return coeff(0) + coeff(1)*t + coeff(2)*pow(t, 2) + coeff(3)*pow(t, 3);
+}
+
+double SplineTrajectory::get_q_dot(Eigen::Vector<double, 4> coeff, double t)
+{
+    return coeff(1) + 2*coeff(2)*t + 3*coeff(3)*pow(t, 2);
+}
+
+double SplineTrajectory::get_q_ddot(Eigen::Vector<double, 4> coeff, double t)
+{
+    return 2*coeff(2) + 6*coeff(3)*t;
+}
+
 
 FootTrajectory::FootTrajectory(double T_step, double dt)
 {
@@ -34,20 +72,6 @@ void FootTrajectory::set_initial_position(Eigen::Vector3d initial_position)
     computed_foot_vel_ = Eigen::Vector3d::Zero();
 }
 
-double FootTrajectory::get_q(Eigen::Vector<double, 4> coeff, double t)
-{
-    return coeff(0) + coeff(1)*t + coeff(2)*pow(t,2) + coeff(3)*pow(t,3);
-}
-
-double FootTrajectory::get_q_dot(Eigen::Vector<double, 4> coeff, double t)
-{
-    return coeff(1) + 2*coeff(2)*t + 3*coeff(3)*pow(t,2);
-}
-
-double FootTrajectory::get_q_ddot(Eigen::Vector<double, 4> coeff, double t)
-{
-    return 2*coeff(2) + 6*coeff(3)*t;
-}
 
 void FootTrajectory::integrate_trajectory_forward(Eigen::Vector3d foot_position, Eigen::Vector3d foot_velocity, Eigen::Vector3d foot_acceleration)
 {
@@ -58,27 +82,6 @@ void FootTrajectory::integrate_trajectory_forward(Eigen::Vector3d foot_position,
     }
 }
 
-Eigen::Vector<double, 4> FootTrajectory::get_spline_coef(double tf, double qi, double qi_dot, double qf, double qf_dot)
-{
-    Eigen::Vector<double, 4> coeff; // a0 a1 a2 a3
-    coeff(0) = qi;
-    coeff(1) = qi_dot;
-
-    Eigen::MatrixXd mat(2,2);
-    mat(0,0) = pow(tf, 3); mat(0,1) = pow(tf, 2);
-    mat(1,0) = 3*pow(tf,2); mat(1,1) = 2*tf;
-    Eigen::MatrixXd mat_inv = mat.inverse();
-
-    Eigen::VectorXd vec(2);
-    vec(0) = qf - qi - qi_dot*tf;
-    vec(1) = qf_dot - qi_dot;
-
-    Eigen::VectorXd vec2 = mat_inv * vec;
-    coeff(3) = vec2(0);
-    coeff(2) = vec2(1);
-
-    return coeff;
-}
 
 void FootTrajectory::get_traj_foot_pos_vel(double T_since_begin_step, Eigen::Vector3d &foot_pos, Eigen::Vector3d &foot_vel, Eigen::Vector3d &foot_acc)
 {
@@ -102,13 +105,13 @@ void FootTrajectory::get_traj_foot_pos_vel(double T_since_begin_step, Eigen::Vec
     if (T_lift_remaining >= 0) {
         // lift foot
         if (T_lift_remaining > fraction*T_lift || !coeff_lift_computed_){
-            coeff_z_spline_lift_ = get_spline_coef(T_lift_remaining, computed_foot_pos_(2), computed_foot_vel_(2), initial_position_(2) + lift_foot_hgt, delta_h_step / T_keep);
+            coeff_z_spline_lift_ = spline_.get_spline_coef(T_lift_remaining, computed_foot_pos_(2), computed_foot_vel_(2), initial_position_(2) + lift_foot_hgt, delta_h_step / T_keep);
             coeff_lift_computed_ = true;
             coeff_lift_timestamp_ = T_lift_remaining;
         }
-        pos(2) = get_q(coeff_z_spline_lift_, coeff_lift_timestamp_ - T_lift_remaining);
-        vel(2) = get_q_dot(coeff_z_spline_lift_, coeff_lift_timestamp_ - T_lift_remaining);
-        acc(2) = get_q_ddot(coeff_z_spline_lift_, coeff_lift_timestamp_ - T_lift_remaining);
+        pos(2) = spline_.get_q(coeff_z_spline_lift_, coeff_lift_timestamp_ - T_lift_remaining);
+        vel(2) = spline_.get_q_dot(coeff_z_spline_lift_, coeff_lift_timestamp_ - T_lift_remaining);
+        acc(2) = spline_.get_q_ddot(coeff_z_spline_lift_, coeff_lift_timestamp_ - T_lift_remaining);
         state_in_traj_ = 0;
     } else if (T_keep_remaining >= 0) {
         // keep foot
@@ -122,13 +125,13 @@ void FootTrajectory::get_traj_foot_pos_vel(double T_since_begin_step, Eigen::Vec
         state_in_traj_ = 2;
         // lower foot
         if (T_lower_remaining > fraction*T_lower || !coeff_lower_computed_){
-            coeff_z_spline_lower_ = get_spline_coef(T_lower_remaining, computed_foot_pos_(2), computed_foot_vel_(2), desired_end_position_(2), -lower_foot_impact_vel);
+            coeff_z_spline_lower_ = spline_.get_spline_coef(T_lower_remaining, computed_foot_pos_(2), computed_foot_vel_(2), desired_end_position_(2), -lower_foot_impact_vel);
             coeff_lower_computed_ = true;
             coeff_lower_timestamp_ = T_lower_remaining;
         }
-        pos(2) = get_q(coeff_z_spline_lower_, coeff_lower_timestamp_ - T_lower_remaining);
-        vel(2) = get_q_dot(coeff_z_spline_lower_, coeff_lower_timestamp_ - T_lower_remaining);
-        acc(2) = get_q_ddot(coeff_z_spline_lower_, coeff_lower_timestamp_ - T_lower_remaining);
+        pos(2) = spline_.get_q(coeff_z_spline_lower_, coeff_lower_timestamp_ - T_lower_remaining);
+        vel(2) = spline_.get_q_dot(coeff_z_spline_lower_, coeff_lower_timestamp_ - T_lower_remaining);
+        acc(2) = spline_.get_q_ddot(coeff_z_spline_lower_, coeff_lower_timestamp_ - T_lower_remaining);
     } else
     {
         // continue lowering
@@ -139,23 +142,23 @@ void FootTrajectory::get_traj_foot_pos_vel(double T_since_begin_step, Eigen::Vec
 
     if (T_remaining > fraction*T_step_ || !coeff_xy_computed_)
     {
-        coeff_x_spline_ = get_spline_coef(T_remaining, computed_foot_pos_(0), computed_foot_vel_(0), desired_end_position_(0), 0);
-        coeff_y_spline_ = get_spline_coef(T_remaining, computed_foot_pos_(1), computed_foot_vel_(1), desired_end_position_(1), 0);
-        pos(0) = get_q(coeff_x_spline_, 0.0);
-        pos(1) = get_q(coeff_y_spline_, 0.0);
-        vel(0) = get_q_dot(coeff_x_spline_, 0.0);
-        vel(1) = get_q_dot(coeff_y_spline_, 0.0);
-        acc(0) = get_q_ddot(coeff_x_spline_, 0.0);
-        acc(1) = get_q_ddot(coeff_y_spline_, 0.0);
+        coeff_x_spline_ = spline_.get_spline_coef(T_remaining, computed_foot_pos_(0), computed_foot_vel_(0), desired_end_position_(0), 0);
+        coeff_y_spline_ = spline_.get_spline_coef(T_remaining, computed_foot_pos_(1), computed_foot_vel_(1), desired_end_position_(1), 0);
+        pos(0) = spline_.get_q(coeff_x_spline_, 0.0);
+        pos(1) = spline_.get_q(coeff_y_spline_, 0.0);
+        vel(0) = spline_.get_q_dot(coeff_x_spline_, 0.0);
+        vel(1) = spline_.get_q_dot(coeff_y_spline_, 0.0);
+        acc(0) = spline_.get_q_ddot(coeff_x_spline_, 0.0);
+        acc(1) = spline_.get_q_ddot(coeff_y_spline_, 0.0);
         coeff_xy_computed_ = true;
         coeff_xy_timestamp_ = T_remaining;
     }else{
-        pos(0) = get_q(coeff_x_spline_, coeff_xy_timestamp_ - T_remaining);
-        pos(1) = get_q(coeff_y_spline_, coeff_xy_timestamp_ - T_remaining);
-        vel(0) = get_q_dot(coeff_x_spline_, coeff_xy_timestamp_ - T_remaining);
-        vel(1) = get_q_dot(coeff_y_spline_, coeff_xy_timestamp_ - T_remaining);
-        acc(0) = get_q_ddot(coeff_x_spline_, coeff_xy_timestamp_ - T_remaining);
-        acc(1) = get_q_ddot(coeff_y_spline_, coeff_xy_timestamp_ - T_remaining);
+        pos(0) = spline_.get_q(coeff_x_spline_, coeff_xy_timestamp_ - T_remaining);
+        pos(1) = spline_.get_q(coeff_y_spline_, coeff_xy_timestamp_ - T_remaining);
+        vel(0) = spline_.get_q_dot(coeff_x_spline_, coeff_xy_timestamp_ - T_remaining);
+        vel(1) = spline_.get_q_dot(coeff_y_spline_, coeff_xy_timestamp_ - T_remaining);
+        acc(0) = spline_.get_q_ddot(coeff_x_spline_, coeff_xy_timestamp_ - T_remaining);
+        acc(1) = spline_.get_q_ddot(coeff_y_spline_, coeff_xy_timestamp_ - T_remaining);
     }
 
     integrate_trajectory_forward(pos, vel, acc);
