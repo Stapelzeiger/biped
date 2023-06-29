15ea82d4251ee364213294c59b12f23760e6d64e
Sorina <lupusorina@yahoo.com>
2023-06-28 22:34:40 -0700

    smooth foot traj

--- capture_point/test/foot_traj_osqp.py
+++ capture_point/test/foot_traj_osqp.py
@@ -1,9 +1,12 @@
 import numpy as np
 import scipy.sparse as spa
 import osqp
+from scipy.sparse import lil_matrix
+from scipy.sparse import csr_matrix
 
-Ts = 0.04
+Ts = 0.3
 dt = 0.01
+height_keep = 0.2
 N = int(Ts/dt)
 print('Nb steps = ', N)
 
@@ -13,59 +16,99 @@ print(nb_total_variables, ' variables')
 # ======== Create P, q matrices ========
 block = np.zeros((3, 3))
 block[-1, -1] = 1
-P = spa.block_diag([block]*N)
-print('P matrix = \n', P.toarray())
+P = spa.block_diag([block]*N, format='csc')
 q = np.zeros((nb_total_variables, 1))
 
 # ======== Create A matrix ========
-block = np.zeros((2, 3))
-block[0, 0] = 1
-block[1, 1] = 1
-A_eq_pos_vel_desired = spa.block_diag([block]*N)
+# boundary points
+A_eq_pos_vel_desired = csr_matrix((4, nb_total_variables))
+A_eq_pos_vel_desired[0, 0] = 1
+A_eq_pos_vel_desired[1, 1] = 1
+A_eq_pos_vel_desired[2, -3] = 1
+A_eq_pos_vel_desired[3, -2] = 1
 
-block_dynamics = np.zeros((2, 5))
+# dynamics points
+block_dynamics = np.zeros((2, 6))
 block_dynamics[0, 0] = 1
 block_dynamics[0, 1] = dt
-block_dynamics[0, 2] = -1
+block_dynamics[0, 3] = -1
 block_dynamics[1, 1] = 1
-block_dynamics[1, 3] = dt
+block_dynamics[1, 2] = dt
 block_dynamics[1, 4] = -1
+A_dynamics_size = (2 * N - 2, 3 * N)
+A_dynamics = lil_matrix(A_dynamics_size)
 
-A_dynamics = spa.block_diag([block_dynamics]*N)
-print(A_dynamics.toarray())
-print(A_dynamics.shape)
+j = 0
+for i in range(int((2 * N - 2)/2)):
+    A_dynamics[j : j + 2, i * 3 : i * 3 + 6] = block_dynamics
+    j = j + 2
 
-# A_total = spa.vstack([A_eq_pos_vel_desired, A_dynamics])
+# limits for vel and acc
+block = np.zeros((2, 3))
+block[0, 1] = 1
+block[1, 2] = 1
+A_limits = spa.block_diag([block]*N, format='csc')
 
-A_total = A_eq_pos_vel_desired.copy()
-print('A matrix = \n', A_total.toarray())
+# raise foot
+T_keep = 33/100*Ts
+T_start_keep = 33/100*Ts
+T_end_keep = T_keep + T_start_keep
+n_keep = int(T_keep/dt)
+n_start_keep = int(T_start_keep/dt)
+n_end_keep = n_keep + n_start_keep
+
+A_keep_foot = lil_matrix((n_keep, nb_total_variables))
+j = n_start_keep
+for i in range(n_keep):
+    A_keep_foot[i, 3 * j] = 1.0
+    j = j + 1
+
+A_total = spa.vstack([A_eq_pos_vel_desired, A_dynamics, A_limits, A_keep_foot], format='csc')
 
 # ======== Create l, u matrices ========
-
+# boundary points
 p0_desired = 0.0
 v0_desired = 0.0
-p_N_desired = 1.0
+p_N_desired = 0.0
 v_N_desired = 0.0
+l_boundary_pts = np.zeros((4, 1))
+u_boundary_pts = np.zeros((4, 1))
+l_boundary_pts[0:2] = np.array([[p0_desired], [v0_desired]])
+u_boundary_pts[0:2] = np.array([[p0_desired], [v0_desired]])
+l_boundary_pts[2:4] = np.array([[p_N_desired], [v_N_desired]])
+u_boundary_pts[2:4] = np.array([[p_N_desired], [v_N_desired]])
 
-l = np.zeros((2*N, 1))
-u = np.zeros((2*N, 1))
+# dynamics points
+l_dynamics = np.zeros((2 * N - 2, 1))
+u_dynamics = np.zeros((2 * N - 2, 1))
 
-l[0:2] = np.array([[p0_desired], [v0_desired]])
-u[0:2] = np.array([[p0_desired], [v0_desired]])
-l[2 * N - 2 :2 * N] = np.array([[p_N_desired], [v_N_desired]])
-u[2 * N - 2 :2 * N] = np.array([[p_N_desired], [v_N_desired]])
+# limits
+v_max = 10.0
+a_max = 20.0
+l_limits = np.zeros((2 * N, 1))
+u_limits = np.zeros((2 * N, 1))
+l_limits[0::2] = -v_max
+l_limits[1::2] = -a_max
+u_limits[0::2] = v_max
+u_limits[1::2] = a_max
 
-l_total = l.copy()
-u_total = u.copy()
+# keep foot
+l_keep = height_keep*np.ones((n_keep, 1))
+u_keep = height_keep*np.ones((n_keep, 1))
+
+l_total = np.vstack([l_boundary_pts, l_dynamics, l_limits, l_keep])
+u_total = np.vstack([u_boundary_pts, u_dynamics, u_limits, u_keep])
 
 prob = osqp.OSQP()
-prob.setup(P, q, A_total, l_total, u_total)
+prob.setup(P, q, A_total, l_total, u_total, verbose=True)
 results = prob.solve()
 
 x_opt = results.x
 pos_opt = x_opt[::3]
 vel_opt = x_opt[1::3]
 acc_opt = x_opt[2::3]
+
+print('pos opt', pos_opt)
 
 import matplotlib.pyplot as plt
 plt.rcParams.update({
@@ -81,12 +124,12 @@ plt.rcParams.update({
 })
 fig, axs = plt.subplots(3, 1, figsize=(10, 10))
 t = np.arange(0, Ts, dt)
-axs[0].plot(t, pos_opt, 'r', label='pos')
+axs[0].plot(t, pos_opt, '.-r', label='pos')
 axs[0].plot(0, p0_desired, 'o')
 axs[0].plot(t[-1], p_N_desired, 'o')
-axs[1].plot(t, vel_opt, 'g', label='vel')
+axs[1].plot(t, vel_opt, '.-g', label='vel')
 axs[1].plot(0, v0_desired, 'o')
 axs[1].plot(t[-1], v_N_desired, 'o')
-axs[2].plot(t, acc_opt, 'b', label='acc')
+axs[2].plot(t, acc_opt, '.-b', label='acc')
 [a.legend() for a in axs]
 plt.show()
