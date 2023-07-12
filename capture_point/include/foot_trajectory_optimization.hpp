bd86c229431cf3e648f1f19b62c1faebf1dc0d0e
Sorina <lupusorina@yahoo.com>
2023-07-12 09:55:42 -0700

    header update for fcnts for the foot opt

--- capture_point/include/foot_trajectory_optimization.hpp
+++ capture_point/include/foot_trajectory_optimization.hpp
@@ -1,14 +1,15 @@
 #ifndef OPTIMIZER_FOOT_TRAJECTORY_H
 #define OPTIMIZER_FOOT_TRAJECTORY_H
-
+#include <Eigen/Sparse>
 #include <Eigen/Dense>
+#include "OsqpEigen/OsqpEigen.h"
 
 class OptimizerFootTrajectory
 {
-private:
-    Eigen::MatrixXd A_matrix_;
-    Eigen::MatrixXd P_matrix_;
+public:
+    Eigen::SparseMatrix<double> P_matrix_;
     Eigen::VectorXd q_vec_;
+    Eigen::SparseMatrix<double> A_matrix_;
     Eigen::VectorXd l_vec_;
     Eigen::VectorXd u_vec_;
     int N_;
@@ -20,14 +21,24 @@ private:
 
     Eigen::Vector3d computed_foot_pos_;
     Eigen::Vector3d computed_foot_vel_;
+    OsqpEigen::Solver solver_;
+    bool run_optimization_;
 
 public:
     OptimizerFootTrajectory(double dt, double Ts);
     ~OptimizerFootTrajectory();
+    bool update_P_and_q_matrices(Eigen::Vector3d opt_weight_pos,
+                                 Eigen::Vector3d opt_weight_vel,
+                                 Eigen::Vector3d p_N_des,
+                                 Eigen::Vector3d v_N_des);
 
-    void set_initial_pos_and_vel(Eigen::Vector3d initial_position);
+    void update_linear_matrix_and_bounds(Eigen::Vector3d p_0_des,
+                                                                  Eigen::Vector3d v_0_des,
+                                                                  double v_max,
+                                                                  double a_max);
 
-    void update_optimization_matrices();
+    void update_nb_variables(double T_since_beginning_of_step);
+
     void create_optimization_pb();
     Eigen::VectorXd solve();
     void integrate_trajectory_forward(Eigen::Vector3d foot_position, Eigen::Vector3d foot_velocity, Eigen::Vector3d foot_acceleration);
