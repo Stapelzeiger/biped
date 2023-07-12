197a9020d13baf0220abf86844a69889a86aa30b
Sorina <lupusorina@yahoo.com>
2023-07-12 08:18:41 -0700

    add test for P and q

--- capture_point/test/foot_traj_optimization_test.cpp
+++ capture_point/test/foot_traj_optimization_test.cpp
@@ -21,6 +21,62 @@ TEST(OptimizerFootTrajectoryTest, IntegrateTrajectoryForwardTest) {
     EXPECT_TRUE(expected_foot_velocity.isApprox(opt.getComputedFootVel()));
 }
 
+
+TEST(OptimizerFootTrajectoryTest, PMatrixQvecTest) {
+    double dt = 0.01;
+    double Ts = 0.02;
+    int N = static_cast<int>(Ts/ dt);
+    OptimizerFootTrajectory opt(dt, Ts);
+    double nb_total_variables_per_coord = 3 * N; // p, v, a
+    double nb_total_variables = 3 * nb_total_variables_per_coord; // x, y, z
+
+    double opt_weight_pos = 1000;
+    double opt_weight_vel = 500;
+    Eigen::Vector3d p_N_des;
+    Eigen::Vector3d v_N_des;
+    p_N_des << 0.5, 0.5, 0.5;
+    v_N_des << 0.0, 0.0, 0.0;
+
+    bool status_update_P_q_matrices = opt.update_P_and_q_matrices(opt_weight_pos, opt_weight_vel, p_N_des, v_N_des);
+
+    Eigen::SparseMatrix<double> P_matrix;
+    Eigen::VectorXd q_vec;
+    P_matrix.resize(nb_total_variables, nb_total_variables);
+    q_vec.resize(nb_total_variables, 1);
+    q_vec.setZero();
+
+    P_matrix.insert(2, 2) = 1;
+    P_matrix.insert(5, 5) = 1;
+    P_matrix.insert(8, 8) = 1;
+    P_matrix.insert(11, 11) = 1;
+    P_matrix.insert(14, 14) = 1;
+    P_matrix.insert(17, 17) = 1;
+
+    P_matrix.insert(3, 3) = opt_weight_pos;
+    P_matrix.insert(4, 4) = opt_weight_vel;
+    P_matrix.insert(9, 9) = opt_weight_pos;
+    P_matrix.insert(10, 10) = opt_weight_vel;
+    P_matrix.insert(15, 15) = opt_weight_pos;
+    P_matrix.insert(16, 16) = opt_weight_vel;
+
+    Eigen::MatrixXd dense_P_matrix = Eigen::MatrixXd(P_matrix);
+    Eigen::MatrixXd dense_P_matrix_opt = Eigen::MatrixXd(opt.P_matrix_);
+
+    q_vec(3) = -2 * opt_weight_pos * p_N_des(0);
+    q_vec(4) = -2 * opt_weight_vel * v_N_des(0);
+    q_vec(9) = -2 * opt_weight_pos * p_N_des(1);
+    q_vec(10) = -2 * opt_weight_vel * v_N_des(1);
+    q_vec(15) = -2 * opt_weight_pos * p_N_des(2);
+    q_vec(16) = -2 * opt_weight_vel * v_N_des(2);
+
+    std::cout << q_vec << std::endl;
+    std::cout << opt.q_vec_ << std::endl;
+
+    EXPECT_TRUE(dense_P_matrix.isApprox(dense_P_matrix_opt));
+    EXPECT_TRUE(q_vec.isApprox(opt.q_vec_));
+    EXPECT_TRUE(status_update_P_q_matrices);
+}
+
 int main(int argc, char** argv)
 {
     testing::InitGoogleTest(&argc, argv);
