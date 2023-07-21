e12b6e18e6b18f0df7a61b5aa1df2b638417b563
Sorina <lupusorina@yahoo.com>
2023-07-21 12:01:41 -0700

    name change

--- capture_point/include/trajectory_optimization.hpp
+++ capture_point/include/trajectory_optimization.hpp
@@ -1,24 +1,25 @@
-#ifndef OPTIMIZER_FOOT_TRAJECTORY_H
-#define OPTIMIZER_FOOT_TRAJECTORY_H
+#ifndef OPTIMIZER_TRAJECTORY_H
+#define OPTIMIZER_TRAJECTORY_H
 #include <Eigen/Sparse>
 #include <Eigen/Dense>
 #include "OsqpEigen/OsqpEigen.h"
 
-class OptimizerFootTrajectory
+class OptimizerTrajectory
 {
 public:
     int N_;
-    double Ts_;
     double dt_;
     int nb_total_variables_per_coord_;
     int nb_total_variables_;
+    double Ts_;
 
     OsqpEigen::Solver solver_;
     bool run_optimization_;
 
 public:
-    OptimizerFootTrajectory(double dt, double Ts);
-    ~OptimizerFootTrajectory();
+    OptimizerTrajectory();
+
+    ~OptimizerTrajectory();
     void get_P_and_q_matrices(Eigen::Vector3d opt_weight_pos,
                                  Eigen::Vector3d opt_weight_vel,
                                  Eigen::Vector3d final_pos,
@@ -39,14 +40,16 @@ public:
                                 Eigen::VectorXd& l_vec,
                                 Eigen::VectorXd& u_vec);
     Eigen::VectorXd solve_optimization_pb();
-    void get_traj_foot_pos_vel(double T_since_begin_step,
-                                Eigen::Vector3d initial_pos,
-                                Eigen::Vector3d initial_vel,
-                                Eigen::Vector3d final_pos,
-                                Eigen::Vector3d final_vel,
-                                std::vector<Eigen::Vector3d> &foot_pos,
-                                std::vector<Eigen::Vector3d> &foot_vel,
-                                std::vector<Eigen::Vector3d> &foot_acc);
+    void get_traj_pos_vel(  double dt,
+                            double Ts,
+                            double T_since_begin_step,
+                            Eigen::Vector3d initial_pos,
+                            Eigen::Vector3d initial_vel,
+                            Eigen::Vector3d final_pos,
+                            Eigen::Vector3d final_vel,
+                            std::vector<Eigen::Vector3d> &pos_vec,
+                            std::vector<Eigen::Vector3d> &vel_vec,
+                            std::vector<Eigen::Vector3d> &acc_vec);
 
 };
 
