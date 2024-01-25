85de25ea67b5fb9b2df2411127cac24762fa9327
Leo <38267386+randomlylelo@users.noreply.github.com>
2024-01-25 11:01:50 -0800

    try make some updates to pendulum expert.

--- sim_mujoco/imit_learning_setup/DAgger/pendulum_expert.py
+++ sim_mujoco/imit_learning_setup/DAgger/pendulum_expert.py
@@ -1,31 +1,40 @@
 import numpy as np
 import gym
-DES_ENERGY = 0.1
-M = 1
+M = 3
 L = 1
 G = 10
-K_energy = 0.5
+K_energy = 0.1
 Kp = 7
 Kd = 2
 
 """Note, modified pendulum environment, won't work off default gym env. Dynamics are wierd. """
 def pendulum_expert(obs):
+    # cosT = cos(theta), sinT = sin(theta), omega = thetadot
     cosT = obs[0]
     sinT = obs[1]
     omega = obs[2]
 
-    energy = 0.5 * (M*L**2) * omega ** 2 - L /2 * (1 - cosT) * M * G
+    # this same equation as drake, except (1 + cosT)
+    # (1 + cosT) is new because adapted to the physics for gym
+    desired_energy = 1.1 * M * G * L
+    energy = 0.5 * M * pow(L * omega, 2) - (M * G * L * (1 + cosT))
+    # energy = 0.5 * (M*L**2) * omega ** 2 - L /2 * (1 - cosT) * M * G
+    # energy = (0.5 * M * (L ** 2) * (omega ** 2)) - (M * G * L * cosT)
     # Close to top, use PD control
     if abs(sinT) < 0.2 and cosT > 0.8:
         u = -Kp * sinT - Kd * omega
     else:
-        u = np.clip(K_energy*(DES_ENERGY - energy) * np.sign(omega), -2, 2)
+        u = K_energy * omega * (desired_energy - energy)
+        # try scaling
+        u = u
+        print(u)
+        u = np.clip(u, -2 , 2)
 
     return [u]
 
 
 def test_expert():
-    env = gym.make("Pendulum-v1")
+    env = gym.make("Pendulum-v1", render_mode='human')
     obs = env.reset()
     obs = obs[0] # SJ: IDK what gym version we should use, but under 0.26.2, obs looks like 
     #    (array([cosT , sinT,  omega ], dtype=float32), {}), so it throws index-out-of-range error without obs = obs[0].
