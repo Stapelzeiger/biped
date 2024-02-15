1026d80bbeefca3500170ad29a7770315769ab22
Sorina Lupu <lupusorina@yahoo.com>
2024-02-14 23:18:36 -0800

    cleanup code for calibration

--- moteus_drv/scripts/calibrate.py
+++ moteus_drv/scripts/calibrate.py
@@ -7,13 +7,20 @@ from sensor_msgs.msg import JointState
 import numpy as np
 import threading
 
+TIME_PERIOD = 0.01
+VEL_MAX = 0.1
+MAX_TRIGGER_EFFORT = 1.5
+COUNTER_TRIGGER = 20
+
 class JointCalibration(Node):
     def __init__(self):
         super().__init__('joint_calibration_publisher')
+
         self.pub_trajectory = self.create_publisher(JointTrajectory, 'joint_trajectory', 10)
         self.joint_states_sub = self.create_subscription(JointState, 'joint_states', self.joint_states_callback, 10)
-        self.timer_period = 0.01 # seconds
+        self.timer_period = TIME_PERIOD # seconds
 
+        # self.joint_names = ['R_YAW', 'R_HAA', 'R_HFE', 'R_KFE', 'L_YAW', 'L_HAA', 'L_HFE', 'L_KFE']
         self.joints_dictionary = { # TODO populate this dict from the config params.yaml
             'joint_names': ['test1'],
             'is_calibrated': [False],
@@ -25,18 +32,20 @@ class JointCalibration(Node):
             'lower_limit': [None],
             'initial_pos': [None],
         }
-        self.calibrated_joints = []
+
         self.counter = 0
         self.all_motors_calibrated = False
-        self.velocity_max = 0.1
-        self.joint_states = None
+
+        self.velocity_max = VEL_MAX
+        self.setpt_pos = self.velocity_max*self.counter/1000
+        self.setpt_vel = self.velocity_max
+
         self.lock = threading.Lock()
         self.timer = self.create_timer(self.timer_period, self.timer_callback)
-        # self.joint_names = ['R_YAW', 'R_HAA', 'R_HFE', 'R_KFE', 'L_YAW', 'L_HAA', 'L_HFE', 'L_KFE']
 
     def joint_states_callback(self, msg):
         with self.lock:
-            for i, joint in enumerate(self.joints_dictionary['joint_names']):
+            for _, joint in enumerate(self.joints_dictionary['joint_names']):
                 if joint in msg.name:
                     idx = msg.name.index(joint)
                     self.joints_dictionary['joint_pos'][idx] = msg.position[idx]
@@ -45,39 +54,49 @@ class JointCalibration(Node):
                     self.joints_dictionary['initial_pos'][idx] = msg.position[idx]
 
     def calibrate_motor(self, joint, idx):
-        setpt_pos = self.velocity_max*self.counter/1000
-        setpt_vel = self.velocity_max
+        with self.lock:
+            msg = JointTrajectory()
+            msg.header.stamp = self.get_clock().now().to_msg()
+            msg.joint_names = [joint]
+            msg.points = [JointTrajectoryPoint()]
+            msg.points[0].positions.append(self.setpt_pos + self.joints_dictionary['initial_pos'][idx])
+            msg.points[0].velocities.append(self.setpt_vel)
+            msg.points[0].effort.append(0)
+            self.pub_trajectory.publish(msg)
 
-        msg = JointTrajectory()
-        msg.header.stamp = self.get_clock().now().to_msg()
-        msg.joint_names = [joint]
-        msg.points = [JointTrajectoryPoint()]
-        msg.points[0].positions.append(setpt_pos + self.joints_dictionary['initial_pos'][idx])
-        msg.points[0].velocities.append(setpt_vel)
-        msg.points[0].effort.append(0)
-        self.pub_trajectory.publish(msg)
-        max_trigger_effort = 1.5
+            joint_effort = self.joints_dictionary['joint_effort'][idx]
 
-        joint_vel = self.joints_dictionary['joint_vel'][idx]
-        joint_effort = self.joints_dictionary['joint_effort'][idx]
-        if np.abs(joint_effort) > max_trigger_effort:
-            self.get_logger().info('joint effort triggered')
-        if (np.abs(joint_effort) > max_trigger_effort and self.velocity_max > 0 and self.counter > 20): # going forward
-            self.get_logger().info(f'Joint {joint} is not moving, record position')
-            self.joints_dictionary['upper_limit'][idx] = self.joints_dictionary['joint_pos'][idx]
-            print('Upper Limit', self.joints_dictionary['upper_limit'])
-            self.velocity_max = -self.velocity_max
-            self.counter = 0
+            # if the joint is not moving and the center pos is not determined, record the position
+            if np.abs(joint_effort) > MAX_TRIGGER_EFFORT and self.joints_dictionary['center_pos'] is None:
+                if self.velocity_max > 0 and self.counter > COUNTER_TRIGGER: # going forward
+                    self.get_logger().info(f'Joint {joint} is not moving, record position')
+                    self.joints_dictionary['upper_limit'][idx] = self.joints_dictionary['joint_pos'][idx]
+                    print('Upper Limit', self.joints_dictionary['upper_limit'])
+                    self.velocity_max = -self.velocity_max
+                    self.counter = 0
 
-        if (np.abs(joint_effort) > max_trigger_effort and self.velocity_max < 0 and self.counter > 20): # going backward
-            self.get_logger().info(f'Joint {joint} is not moving, record position')
-            self.joints_dictionary['lower_limit'][idx] = self.joints_dictionary['joint_pos'][idx]
-            print('Lower Limit', self.joints_dictionary['lower_limit'])
-            self.velocity_max = -self.velocity_max
-            # goto center position
-            self.joints_dictionary['is_calibrated'][idx] = True
-            self.joints_dictionary['center_pos'][idx] = (self.joints_dictionary['upper_limit'][idx] + self.joints_dictionary['lower_limit'][idx])/2
-            self.counter = 0
+                if (self.velocity_max < 0 and self.counter > COUNTER_TRIGGER): # going backward
+                    self.get_logger().info(f'Joint {joint} is not moving, record position')
+                    self.joints_dictionary['lower_limit'][idx] = self.joints_dictionary['joint_pos'][idx]
+                    print('Lower Limit', self.joints_dictionary['lower_limit'])
+                    self.velocity_max = -self.velocity_max
+                    # set center position
+                    self.joints_dictionary['center_pos'] = (self.joints_dictionary['upper_limit'][idx] + self.joints_dictionary['lower_limit'][idx])/2
+                    self.counter = 0
+
+            # center pos is determined, move the joint to the center pos
+            if self.joints_dictionary['center_pos'][idx] is not None and abs(self.joints_dictionary['joint_pos'][idx] - self.joints_dictionary['center_pos'][idx]) > 0.01:
+                # drive the robot to the center position
+                self.setpt_pos = self.joints_dictionary['center_pos'][idx]
+                self.setpt_vel = self.velocity_max
+            else:
+                self.setpt_pos = self.velocity_max*self.counter*(TIME_PERIOD*0.1)
+                self.setpt_vel = self.velocity_max
+
+            # verify the center position was achieved
+            if self.joints_dictionary['center_pos'][idx] is not None and abs(self.joints_dictionary['joint_pos'][idx] - self.joints_dictionary['center_pos'][idx]) < 0.01:
+                self.joints_dictionary['is_calibrated'][idx] = True
+                self.get_logger().info(f'Joint {joint} is calibrated')
 
     def timer_callback(self):
         if self.joints_dictionary['is_calibrated'] == [True]*len(self.joints_dictionary['joint_names']):
