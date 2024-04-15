971548d33d940bd5da344f1567933ea701dc8f26
Leo <38267386+randomlylelo@users.noreply.github.com>
2024-04-15 11:11:30 -0700

    feat: create autoencoder folder and dataprocessing folder, write some scaffolding code for autoencoder.

---
+++ sim_mujoco/imit_learning_setup/autoencoder/autoencoder.py
@@ -0,0 +1,39 @@
+import torch
+import torch.nn as nn
+from torch.utils.tensorboard import SummaryWriter
+
+class Autoencoder(nn.Module):
+    def __init__(self, states):
+        self.encoder = nn.Sequential(
+            nn.Linear(states, 128),
+            nn.ReLU(True),
+            nn.Linear(128, 64),
+            nn.ReLU(True),
+            nn.Linear(64, 12),
+            nn.ReLU(True),
+            nn.Linear(12, 3))
+        self.decoder = nn.Sequential(
+            nn.Linear(3, 12),
+            nn.ReLU(True),
+            nn.Linear(12, 64),
+            nn.ReLU(True),
+            nn.Linear(64, 128),
+            nn.ReLU(True),
+            nn.Linear(128, states))
+
+        self.device = (
+            "cuda"
+            if torch.cuda.is_available()
+            else "mps"
+            if torch.backends.mps.is_available()
+            else "cpu"
+        )
+
+    def forward(self, x):
+        encoded = self.encoder(x)
+        decoded = self.decoder(encoded)
+        return decoded
+
+    
+    def train(self, epochs):
+        pass
