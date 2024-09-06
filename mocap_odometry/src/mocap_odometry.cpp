c43ba956777cf5d2b54fa18ace6c17a30de0e30c
Sorina <lupusorina@yahoo.com>
2024-09-06 07:57:45 -0700

    renaming, fix bugs and test in sim

--- mocap_odometry/src/mocap_odometry.cpp
+++ mocap_odometry/src/mocap_odometry.cpp
@@ -22,6 +22,17 @@
 using namespace std::placeholders;
 using namespace std::chrono_literals;
 
+struct IMUData {
+    sensor_msgs::msg::Imu imu_msg;
+    rclcpp::Time timestamp;
+
+    IMUData(sensor_msgs::msg::Imu msg, rclcpp::Time ts)
+        : imu_msg(msg), timestamp(ts) {}
+
+    bool operator<(const IMUData& other) const {
+        return timestamp < other.timestamp;
+    }
+};
 
 Eigen::Matrix3d cross_product_matrix(Eigen::Vector3d v)
 {
@@ -49,18 +60,24 @@ class IMUPoseEKF
     {
       // nominal states
       att_ = att_0;
+      pos_I_ = Eigen::Vector3d::Zero();
       vel_I_ = Eigen::Vector3d::Zero();
       acc_bias_ = Eigen::Vector3d::Zero();
       gyro_bias_ = Eigen::Vector3d::Zero();
       // EKF states
-      Eigen::Array<double, 1, 12> P_diag;
-      P_diag << vel_std_0_, vel_std_0_, vel_std_0_,
-          att_std_0_, att_std_0_, att_std_0_,
-          acc_bias_std_0_, acc_bias_std_0_, acc_bias_std_0_,
-          gyro_bias_std_0_, gyro_bias_std_0_, gyro_bias_std_0_;
+      Eigen::Array<double, 1, NX> P_diag;
+      P_diag.setZero();
+      for (int i = 0; i < 3; i++)
+      {
+        P_diag(XVEL + i) = vel_std_0_;
+        P_diag(XATT + i) = att_std_0_;
+        P_diag(XACC_BIAS + i) = acc_bias_std_0_;
+        P_diag(XGYRO_BIAS + i) = gyro_bias_std_0_;
+        P_diag(XPOS + i) = pos_std_0_;
+      }
       P_ = P_diag.pow(2.0).matrix().asDiagonal();
+
       // output states
-      pos_ = Eigen::Vector3d::Zero();
       omega_ = Eigen::Vector3d::Zero();
     }
 
@@ -75,17 +92,18 @@ class IMUPoseEKF
       dq.normalize();
 
       auto R = att_.toRotationMatrix();
-      Eigen::Matrix<double, 12, 12> F_x;
+      Eigen::Matrix<double, NX, NX> F_x;
       F_x.setIdentity();
-      F_x.block(0, 3, 3, 3) = -dt * R * cross_product_matrix(acc);
-      F_x.block(0, 6, 3, 3) = -dt * R;
-      F_x.block(3, 3, 3, 3) =  (dq.toRotationMatrix()).transpose();
-      F_x.block(3, 9, 3, 3) = -dt * Eigen::MatrixXd::Identity(3, 3);
+      F_x.block(XVEL, XATT, 3, 3) = -dt * R * cross_product_matrix(acc);
+      F_x.block(XVEL, XACC_BIAS, 3, 3) = -dt * R;
+      F_x.block(XATT, XATT, 3, 3) =  (dq.toRotationMatrix()).transpose();
+      F_x.block(XATT, XGYRO_BIAS, 3, 3) = -dt * Eigen::MatrixXd::Identity(3, 3);
+      F_x.block(XPOS, XVEL, 3, 3) = dt * Eigen::MatrixXd::Identity(3, 3);
 
-      Eigen::Matrix<double, 12, 12> Fi;
+      Eigen::Matrix<double, NX, NX> Fi;
       Fi.setIdentity();
 
-      Eigen::Matrix<double, 12, 12> Qi;
+      Eigen::Matrix<double, NX, NX> Qi;
       Qi.setZero();
       double acc_noise_var = acc_noise_density_ * acc_noise_density_ / dt;
       double velocity_var = acc_noise_var * dt * dt;
@@ -95,18 +113,19 @@ class IMUPoseEKF
       double gyro_bias_var = gyro_bias_random_walk_ * gyro_bias_random_walk_ * dt;
       for (int i = 0; i < 3; i++)
       {
-        Qi(i, i) = velocity_var;
-        Qi(i + 3, i + 3) = att_var;
-        Qi(i + 6, i + 6) = acc_bias_var;
-        Qi(i + 9, i + 9) = gyro_bias_var;
+        Qi(XVEL + i, XVEL + i) = velocity_var;
+        Qi(XATT + i, XATT + i) = att_var;
+        Qi(XACC_BIAS + i, XACC_BIAS + i) = acc_bias_var;
+        Qi(XGYRO_BIAS + i, XGYRO_BIAS + i) = gyro_bias_var;
+        Qi(XPOS + i, XPOS + i) = 0.0;
       }
-      
+
       // update nominal system
       att_ = att_ * dq;
       att_.normalize();
       const Eigen::Vector3d gravity_I(0, 0, -9.81);
       vel_I_ += dt * (R * acc + gravity_I);
-      pos_ += dt * vel_I_;
+      pos_I_ += dt * vel_I_;
       // update EKF (error state is zero)
       P_ = F_x * P_ * F_x.transpose() + Fi * Qi * Fi.transpose();
       // std::cout << "imu update: " << std::endl;
@@ -116,33 +135,111 @@ class IMUPoseEKF
       // std::cout << " vel_I: " << vel_I_.transpose() << std::endl;
     }
 
-    /* contact velocity, B = imu frame */
-    Eigen::Vector3d compute_contact_residual(const Eigen::Vector3d &pos_B, const Eigen::Vector3d &vel_B) const
+  void pos_measurement(const Eigen::Vector3d &pos_B, const Eigen::Vector3d &pos_I_meas, const Eigen::Matrix3d &R_cov,
+      Eigen::Vector3d &h_out, Eigen::Matrix3d &h_cov_out)
     {
+      // h(x) = R * pos_B + pos_I
       auto R = att_.toRotationMatrix();
-      Eigen::Vector3d vel_B_total = vel_B + omega_.cross(pos_B);
-      auto h = R * vel_B_total + vel_I_;
-      return -h;
+      Eigen::Vector3d h = R * pos_B + pos_I_;
+      // h = (R * R(delta_att) * pos_B) + pos_I = R * (I + S(delta_att)) * pos_B + pos_I + O(|delta_att|^2)
+      Eigen::Matrix<double, 3, NX> H;
+      H.setZero();
+      // partial h / partial delta_vel_I = [0, 0, 0]
+      // partial h / partial delta_att = -R * S(pos_B)
+      H.block(0, XATT, 3, 3) = -R * cross_product_matrix(pos_B);
+      // partial h / partial delta_acc_bias = [0, 0, 0]
+      // partial h / partial delta_gyro_bias = [0, 0, 0]
+      // partial h / partial delta_pos_I = I
+      H.block(0, XPOS, 3, 3) = Eigen::MatrixXd::Identity(3, 3);
+
+      auto h_cov = H * P_ * H.transpose() + R_cov;
+      auto K = P_ * H.transpose() * h_cov.inverse();
+      Eigen::Matrix<double, NX, 1> delta_x = K * (pos_I_meas - h);
+      P_ = (Eigen::MatrixXd::Identity(NX, NX) - K * H) * P_;
+
+      // update nominal system
+      pos_I_ += delta_x.segment<3>(XPOS);
+      vel_I_ += delta_x.segment<3>(XVEL);
+      Eigen::Quaterniond delta_att;
+      delta_att.vec() = delta_x.block(XATT, 0, 3, 1) * 0.5;
+      delta_att.w() = 1;
+      att_ = att_ * delta_att;
+      att_.normalize();
+      acc_bias_ += delta_x.segment<3>(XACC_BIAS);
+      gyro_bias_ += delta_x.segment<3>(XGYRO_BIAS);
+
+      // debug outputs
+      h_out = h;
+      h_cov_out = h_cov;
     }
 
-    void get_state(Eigen::Vector3d &pos, Eigen::Vector3d &vel_I, Eigen::Quaterniond &att, Eigen::Vector3d &omega)
+    void att_measurement(const Eigen::Quaterniond &quat_meas, const Eigen::Matrix3d &R_cov, Eigen::Vector3d &h_out, Eigen::Matrix3d &h_cov_out)
     {
-      pos = pos_;
-      att = att_;
+      // h(x) = delta_att
+      Eigen::Vector3d h = Eigen::Vector3d::Zero();
+      Eigen::Matrix<double, 3, NX> H;
+      H.setZero();
+      // partial h / partial delta_vel_I = [0, 0, 0]
+      // partial h / partial delta_att = 
+      H.block(0, XATT, 3, 3) = Eigen::MatrixXd::Identity(3, 3);
+      // partial h / partial delta_acc_bias = [0, 0, 0]
+      // partial h / partial delta_gyro_bias = [0, 0, 0]
+      // partial h / partial delta_pos_I = [0, 0, 0]
+
+      auto q_nom_to_I = att_;
+      auto q_meas_to_I = quat_meas;
+      auto q_meas_to_nom = q_nom_to_I.conjugate() * q_meas_to_I;
+      Eigen::Vector3d y;
+      if (q_meas_to_nom.w() > 0) {
+        y = 2 * q_meas_to_nom.vec();
+      } else {
+        y = -2 * q_meas_to_nom.vec();
+      }
+      auto h_cov = H * P_ * H.transpose() + R_cov;
+      auto K = P_ * H.transpose() * h_cov.inverse();
+      Eigen::Matrix<double, NX, 1> delta_x = K * (y - h);
+      P_ = (Eigen::MatrixXd::Identity(NX, NX) - K * H) * P_;
+
+      // update nominal system
+      pos_I_ += delta_x.segment<3>(XPOS);
+      vel_I_ += delta_x.segment<3>(XVEL);
+      Eigen::Quaterniond delta_att;
+      delta_att.vec() = delta_x.block(XATT, 0, 3, 1) * 0.5;
+      delta_att.w() = 1;
+      att_ = att_ * delta_att;
+      att_.normalize();
+      acc_bias_ += delta_x.segment<3>(XACC_BIAS);
+      gyro_bias_ += delta_x.segment<3>(XGYRO_BIAS);
+
+      // debug outputs
+      h_out = h;
+      h_cov_out = h_cov;
+    }
+
+    void get_state(Eigen::Vector3d &pos_I, Eigen::Vector3d &vel_I, Eigen::Quaterniond &att, Eigen::Vector3d &omega)
+    {
       vel_I = vel_I_;
+      att = att_;
       omega = omega_;
+      pos_I = pos_I_;
     }
 
 private:
   // nominal states
   Eigen::Quaterniond att_;
+  Eigen::Vector3d pos_I_;
   Eigen::Vector3d vel_I_;
   Eigen::Vector3d acc_bias_;
   Eigen::Vector3d gyro_bias_;
   // EKF states
-  Eigen::Matrix<double, 12, 12> P_; // [d_vel, d_att, d_acc_bias, d_gyro_bias]
+  static const int NX = 15;
+  static const int XVEL = 0;
+  static const int XATT = 3;
+  static const int XACC_BIAS = 6;
+  static const int XGYRO_BIAS = 9;
+  static const int XPOS = 12;
+  Eigen::Matrix<double, NX, NX> P_; // [d_vel, d_att, d_acc_bias, d_gyro_bias, d_pos]
   // output states
-  Eigen::Vector3d pos_;
   Eigen::Vector3d omega_;
 
 public:
@@ -152,6 +249,7 @@ public:
   double acc_bias_random_walk_;  // [m/s2 / sqrt(s)]
   double gyro_bias_random_walk_;  // [rad/s / sqrt(s)]
   // initial covariance
+  double pos_std_0_;
   double vel_std_0_;
   double att_std_0_;
   double acc_bias_std_0_;
@@ -160,15 +258,16 @@ public:
 };
 
 
-class ViconOdometry : public rclcpp::Node
+class MocapOdometry : public rclcpp::Node
 {
 public:
-  ViconOdometry()
-  : Node("vicon_odometry")
+  MocapOdometry()
+  : Node("mocap_odometry")
   {
 
     base_link_frame_id_ = this->declare_parameter<std::string>("base_link_frame_id", "base_link");
-    odom_frame_id_ = this->declare_parameter<std::string>("odom_frame_id", "odom");
+    base_link_mocap_frame_id_ = this->declare_parameter<std::string>("base_link_mocap_frame_id", "base_link_mocap");
+    odom_frame_id_ = this->declare_parameter<std::string>("odom_frame_id", "world");
     this->declare_parameter<bool>("publish_tf", false);
 
     // load noise parameters
@@ -186,13 +285,17 @@ public:
 
     odom_pub_ = this->create_publisher<nav_msgs::msg::Odometry>("~/odom", 10);
     ekf_innovations_marker_pub_ = this->create_publisher<visualization_msgs::msg::MarkerArray>("~/ekf_innovations", 10);
+    
     tf_broadcaster_ = std::make_shared<tf2_ros::TransformBroadcaster>(this);
 
     imu_sub_ = this->create_subscription<sensor_msgs::msg::Imu>(
-      "~/imu", 10, std::bind(&ViconOdometry::imu_cb, this, _1));
+      "~/imu", 10, std::bind(&MocapOdometry::imu_cb, this, _1));
 
     reset_sub_ = this->create_subscription<std_msgs::msg::Bool>(
-      "~/reset", 10, std::bind(&ViconOdometry::reset_cb, this, _1));
+      "~/reset", 10, std::bind(&MocapOdometry::reset_cb, this, _1));
+
+    mocap_sub_ = this->create_subscription<geometry_msgs::msg::PoseStamped>(
+      "~/mocap/pose", 10, std::bind(&MocapOdometry::mocap_cb, this, _1));
 
   }
 
@@ -207,6 +310,89 @@ private:
     }
   }
 
+  void mocap_cb(const geometry_msgs::msg::PoseStamped::SharedPtr msg)
+  {
+    // get mocap transform (cached if the frame id is the same)
+    if (msg->header.frame_id != mocap_frame_id_)
+    {
+      if (msg->header.frame_id == "")
+      {
+        RCLCPP_WARN_THROTTLE(this->get_logger(), *this->get_clock(), 1000 /* ms */, "mocap message has no frame_id, skipping");
+        return;
+      }
+      try {
+        geometry_msgs::msg::TransformStamped T_MOCAP_to_I = tf_buffer_->lookupTransform(odom_frame_id_, msg->header.frame_id, rclcpp::Time(0));
+        p_MOCAP_in_I_ << T_MOCAP_to_I.transform.translation.x, T_MOCAP_to_I.transform.translation.y, T_MOCAP_to_I.transform.translation.z;
+        q_MOCAP_to_I_ = Eigen::Quaterniond(T_MOCAP_to_I.transform.rotation.w,
+                                            T_MOCAP_to_I.transform.rotation.x,
+                                            T_MOCAP_to_I.transform.rotation.y,
+                                            T_MOCAP_to_I.transform.rotation.z);
+
+
+      } catch (tf2::TransformException &ex) {
+        RCLCPP_WARN(this->get_logger(), "Could not get transform from %s to %s, skipping", msg->header.frame_id.c_str(), imu_frame_id_.c_str());
+        return;
+      }
+      RCLCPP_WARN_SKIPFIRST(this->get_logger(), "Mocap frame id changed from %s to %s", msg->header.frame_id.c_str(), msg->header.frame_id.c_str());
+      mocap_frame_id_ = msg->header.frame_id;
+    }
+
+    try {
+        geometry_msgs::msg::TransformStamped T_MOCAP_to_IMU = tf_buffer_->lookupTransform(imu_frame_id_, base_link_mocap_frame_id_, rclcpp::Time(0));
+        p_MOCAP_in_IMU_ << T_MOCAP_to_IMU.transform.translation.x, T_MOCAP_to_IMU.transform.translation.y, T_MOCAP_to_IMU.transform.translation.z;
+      } catch (tf2::TransformException &ex) {
+        RCLCPP_WARN(this->get_logger(), "Could not get transform from %s to %s, skipping", base_link_mocap_frame_id_.c_str(), imu_frame_id_.c_str());
+        return;
+    }
+
+    // message time checks
+    auto new_time = rclcpp::Time(msg->header.stamp);
+    if (time_ == rclcpp::Time(0, 0, RCL_ROS_TIME)) {  // first message
+      time_ = new_time;
+      return;
+    }
+    double dt = (new_time - time_).seconds();
+    if (dt < 0) {
+      RCLCPP_WARN(this->get_logger(), "Mocap messages are not in order, skipping");
+      return;
+    }
+    time_ = new_time;
+    if (dt > 0.1){
+      RCLCPP_WARN(this->get_logger(), "Mocap messages gap");
+      return;
+    }
+
+    Eigen::Vector3d pos_I;
+    pos_I << msg->pose.position.x, msg->pose.position.y, msg->pose.position.z;
+    
+    // update EKF for position measurement
+    Eigen::Matrix3d R_cov;
+    R_cov.setZero();
+    R_cov(0, 0) = 0.1; // TODO parameters
+    R_cov(1, 1) = 0.1;
+    R_cov(2, 2) = 0.1;
+    Eigen::Vector3d h_out; // todo publish for plotting
+    Eigen::Matrix3d h_cov_out;
+    ekf_.pos_measurement(p_MOCAP_in_IMU_, pos_I, R_cov, h_out, h_cov_out);
+
+    // update EKF for attitude measurement
+    Eigen::Quaterniond att_MOCAP_to_I;
+    att_MOCAP_to_I.w() = msg->pose.orientation.w;
+    att_MOCAP_to_I.x() = msg->pose.orientation.x;
+    att_MOCAP_to_I.y() = msg->pose.orientation.y;
+    att_MOCAP_to_I.z() = msg->pose.orientation.z;
+
+    Eigen::Matrix3d R_cov_att;
+    R_cov_att.setZero();
+    R_cov_att(0, 0) = 0.1; // TODO parameters
+    R_cov_att(1, 1) = 0.1;
+    R_cov_att(2, 2) = 0.1;
+    Eigen::Vector3d h_out_att; // todo publish for plotting
+    Eigen::Matrix3d h_cov_out_att;
+    ekf_.att_measurement(att_MOCAP_to_I, R_cov_att, h_out_att, h_cov_out_att);
+  }
+
+
   void imu_cb(const sensor_msgs::msg::Imu::SharedPtr msg)
   {
     // get imu transform (cached if the frame id is the same)
@@ -219,7 +405,10 @@ private:
       }
       try {
         geometry_msgs::msg::TransformStamped T_IMU_to_BL = tf_buffer_->lookupTransform(base_link_frame_id_, msg->header.frame_id, rclcpp::Time(0));
-        q_IMU_to_BL_ = Eigen::Quaterniond(T_IMU_to_BL.transform.rotation.w, T_IMU_to_BL.transform.rotation.x, T_IMU_to_BL.transform.rotation.y, T_IMU_to_BL.transform.rotation.z);
+        q_IMU_to_BL_ = Eigen::Quaterniond(T_IMU_to_BL.transform.rotation.w,
+                                          T_IMU_to_BL.transform.rotation.x,
+                                          T_IMU_to_BL.transform.rotation.y,
+                                          T_IMU_to_BL.transform.rotation.z);
         p_IMU_in_BL_ << T_IMU_to_BL.transform.translation.x, T_IMU_to_BL.transform.translation.y, T_IMU_to_BL.transform.translation.z;
         ekf_.reset(q_IMU_to_BL_);
       } catch (tf2::TransformException &ex) {
@@ -251,6 +440,7 @@ private:
       return;
     }
 
+
     // EKF time update
     Eigen::Vector3d acc_m;
     Eigen::Vector3d gyro_m;
@@ -266,12 +456,14 @@ private:
     Eigen::Vector3d posIMU_I, velIMU_I, omegaIMU_IMU;
     Eigen::Quaterniond att_IMU_to_I;
     ekf_.get_state(posIMU_I, velIMU_I, att_IMU_to_I, omegaIMU_IMU);
+
     // transform from imu frame to base_link frame
     Eigen::Quaterniond att_BL_to_I = att_IMU_to_I * q_IMU_to_BL_.conjugate();
     Eigen::Vector3d posBL_I = posIMU_I - att_BL_to_I * p_IMU_in_BL_;
     Eigen::Vector3d omegaBL_BL = q_IMU_to_BL_ * omegaIMU_IMU;
     Eigen::Vector3d velIMU_BL = att_BL_to_I.conjugate() * velIMU_I;
     Eigen::Vector3d velBL_BL = velIMU_BL - omegaBL_BL.cross(p_IMU_in_BL_);
+
     // publish odometry
     nav_msgs::msg::Odometry odom;
     odom.header.stamp = msg->header.stamp;
@@ -315,12 +507,21 @@ private:
 
   std::string imu_frame_id_;
   std::string base_link_frame_id_;
+  std::string base_link_mocap_frame_id_;
   std::string odom_frame_id_;
+  std::string mocap_frame_id_;
   Eigen::Quaterniond q_IMU_to_BL_;
   Eigen::Vector3d p_IMU_in_BL_;
+  Eigen::Vector3d p_MOCAP_in_IMU_;
+  Eigen::Quaterniond q_MOCAP_to_I_;
+  Eigen::Vector3d p_MOCAP_in_I_;
+
+  bool publish_tf_;
 
   rclcpp::Subscription<sensor_msgs::msg::Imu>::SharedPtr imu_sub_;
   rclcpp::Subscription<std_msgs::msg::Bool>::SharedPtr reset_sub_;
+  rclcpp::Subscription<geometry_msgs::msg::PoseStamped>::SharedPtr mocap_sub_;
+
   rclcpp::Publisher<nav_msgs::msg::Odometry>::SharedPtr odom_pub_;
   std::shared_ptr<tf2_ros::TransformBroadcaster> tf_broadcaster_;
   rclcpp::Publisher<visualization_msgs::msg::MarkerArray>::SharedPtr ekf_innovations_marker_pub_;
@@ -333,7 +534,7 @@ private:
 int main(int argc, char * argv[])
 {
   rclcpp::init(argc, argv);
-  rclcpp::spin(std::make_shared<ViconOdometry>());
+  rclcpp::spin(std::make_shared<MocapOdometry>());
   rclcpp::shutdown();
   return 0;
 }
