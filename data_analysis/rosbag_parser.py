2a7c3f0e55b0c3f580f9fb9ad67ee4c4d5e60aa3
Sorina <lupusorina@yahoo.com>
2024-11-17 22:10:23 -0800

    add dcm

--- data_analysis/rosbag_parser.py
+++ data_analysis/rosbag_parser.py
@@ -15,12 +15,15 @@ from rclpy.serialization import deserialize_message
 
 from sensor_msgs.msg import JointState
 from trajectory_msgs.msg import JointTrajectory, JointTrajectoryPoint
+from visualization_msgs.msg import Marker
+from geometry_msgs.msg import Vector3Stamped
 
 
 # Parse the arguments.
 parser = argparse.ArgumentParser(description="processes ROS 2 Bag directory path.")
 parser.add_argument('--path', type=str, help="ROS 2 Bag directory path")
 parser.add_argument('--extract_csvs', action='store_true', help="Extracts the messages to CSV files")
+parser.add_argument('--plot_data', action='store_true', help="Plots the data")
 args = parser.parse_args()
 rosbag_path = args.path
 
@@ -45,14 +48,24 @@ if args.extract_csvs:
     reader.open(options, converter_options)
 
     # Create a dataframe for each joint state.
-    header_df_state = 'timestamp', 'name', 'pos', 'vel', 'eff'
-    df_state = pd.DataFrame(columns=header_df_state)
-    dfs_state = [df_state for _ in range(len(list_joints))]
+    header_df_joint_state = 'timestamp', 'name', 'pos', 'vel', 'eff'
+    df_joint_state = pd.DataFrame(columns=header_df_joint_state)
+    dfs_joint_state = [df_joint_state for _ in range(len(list_joints))]
 
     # Create a dataframe for each joint trajectory.
-    header_df_traj = 'timestamp', 'name', 'pos_des', 'vel_des', 'eff_des'
-    df_traj = pd.DataFrame(columns=header_df_traj)
-    dfs_traj = [df_traj for _ in range(len(list_joints))]
+    header_df_joint_traj = 'timestamp', 'name', 'pos_des', 'vel_des', 'eff_des'
+    df_joint_traj = pd.DataFrame(columns=header_df_joint_traj)
+    dfs_joint_traj = [df_joint_traj for _ in range(len(list_joints))]
+
+    # Create a dataframe for the capture point DCM.
+    header_df_dcm_des = 'timestamp', 'desired_dcm_x', 'desired_dcm_y',
+    header_df_dcm_next = 'timestamp', 'next_footstep_x', 'next_footstep_y'
+    header_df_dcm_predicted = 'timestamp', 'predicted_dcm_x', 'predicted_dcm_y'
+    header_df_dcm = 'timestamp', 'dcm_x', 'dcm_y'
+    df_dcm_desired = pd.DataFrame(columns=header_df_dcm_des)
+    df_dcm = pd.DataFrame(columns=header_df_dcm)
+    df_dcm_next = pd.DataFrame(columns=header_df_dcm_next)
+    df_dcm_predicted = pd.DataFrame(columns=header_df_dcm_predicted)
 
     t_init = None
     counter = 0.0
@@ -67,8 +80,8 @@ if args.extract_csvs:
                         joint_index = msg.joint_names.index(joint_name)
                         time = msg.header.stamp.sec + msg.header.stamp.nanosec * 1e-9
                         row = [time, msg.joint_names[joint_index], msg.points[0].positions[joint_index], msg.points[0].velocities[joint_index], msg.points[0].effort[joint_index]]
-                        dfs_traj[list_joints.index(joint_name)] = pd.concat(
-                            [dfs_traj[list_joints.index(joint_name)], pd.DataFrame([row], columns=header_df_traj)],
+                        dfs_joint_traj[list_joints.index(joint_name)] = pd.concat(
+                            [dfs_joint_traj[list_joints.index(joint_name)], pd.DataFrame([row], columns=header_df_joint_traj)],
                             ignore_index=True
                         )
 
@@ -80,75 +93,134 @@ if args.extract_csvs:
                         joint_index = msg.name.index(joint_name)
                         time = msg.header.stamp.sec + msg.header.stamp.nanosec * 1e-9
                         row = [time, msg.name[joint_index], msg.position[joint_index], msg.velocity[joint_index], msg.effort[joint_index]]
-                        dfs_state[list_joints.index(joint_name)] = pd.concat(
-                            [dfs_state[list_joints.index(joint_name)], pd.DataFrame([row], columns=header_df_state)],
+                        dfs_joint_state[list_joints.index(joint_name)] = pd.concat(
+                            [dfs_joint_state[list_joints.index(joint_name)], pd.DataFrame([row], columns=header_df_joint_state)],
                             ignore_index=True
                         )
 
+            if topic == '/capture_point/markers_next_footstep':
+                msg = deserialize_message(msg, Marker)
+                time = msg.header.stamp.sec + msg.header.stamp.nanosec * 1e-9
+                row = [time, msg.pose.position.x, msg.pose.position.y]
+                df_dcm_next = pd.concat([df_dcm_next, pd.DataFrame([row], columns=header_df_dcm_next)], ignore_index=True)
+
+            if topic == '/capture_point/markers_dcm':
+                msg = deserialize_message(msg, Marker)
+                time = msg.header.stamp.sec + msg.header.stamp.nanosec * 1e-9
+                row = [time, msg.pose.position.x, msg.pose.position.y]
+                df_dcm = pd.concat([df_dcm, pd.DataFrame([row], columns=header_df_dcm)], ignore_index=True)
+
+            if topic == '/capture_point/desired_dcm':
+                msg = deserialize_message(msg, Vector3Stamped)
+                time = msg.header.stamp.sec + msg.header.stamp.nanosec * 1e-9
+                row_data = [time, msg.vector.x, msg.vector.y]
+                df_dcm_desired = pd.concat([df_dcm_desired, pd.DataFrame([row_data], columns=header_df_dcm)], ignore_index=True)
+
+            if topic == '/capture_point/predicted_dcm':
+                msg = deserialize_message(msg, Vector3Stamped)
+                time = msg.header.stamp.sec + msg.header.stamp.nanosec * 1e-9
+                row_data = [time, msg.vector.x, msg.vector.y]
+                df_dcm_predicted = pd.concat([df_dcm_predicted, pd.DataFrame([row_data], columns=header_df_dcm_predicted)], ignore_index=True)
+
             counter += 1
+
             pbar.update(1)
 
     print("Finished processing messages")
     print(f"Number of messages processed: {counter}")
 
     # Save the csvs in the same directory as the rosbag.
-    for i, df in enumerate(dfs_state):
+    for i, df in enumerate(dfs_joint_state):
         df.to_csv(rosbag_path + f'/{list_joints[i]}_state.csv', index=False)
 
-    for i, df in enumerate(dfs_traj):
+    for i, df in enumerate(dfs_joint_traj):
         df.to_csv(rosbag_path + f'/{list_joints[i]}_traj.csv', index=False)
 
-# Load the csvs.
-dfs_state = []
-dfs_traj = []
+    df_dcm_desired.to_csv(rosbag_path + '/dcm_des.csv', index=False)
+    df_dcm_next.to_csv(rosbag_path + '/dcm_next.csv', index=False)
+    df_dcm_predicted.to_csv(rosbag_path + '/dcm_predicted.csv', index=False)
+    df_dcm.to_csv(rosbag_path + '/dcm.csv', index=False)
 
-DES_TIME_CUT = [40.0, 60.0]
 
-for joint_name in list_joints:
-    df_state = pd.read_csv(rosbag_path + f'/{joint_name}_state.csv')
-    df_traj = pd.read_csv(rosbag_path + f'/{joint_name}_traj.csv')
-    df_state['timestamp'] = df_state['timestamp'] - df_state['timestamp'][0]
-    df_traj['timestamp'] = df_traj['timestamp'] - df_traj['timestamp'][0]
+def plot_data(rosbag_path):
+    # Load the csvs.
+    dfs_joint_state = []
+    dfs_joint_traj = []
 
-    df_state_filtered = df_state[(df_state['timestamp'] >= DES_TIME_CUT[0]) & (df_state['timestamp'] <= DES_TIME_CUT[1])]
-    df_traj_filtered = df_traj[(df_traj['timestamp'] >= DES_TIME_CUT[0]) & (df_traj['timestamp'] <= DES_TIME_CUT[1])]
-    dfs_state.append(df_state_filtered)
-    dfs_traj.append(df_traj_filtered)
+    DES_TIME_CUT = None
 
-# Plot the joint positions.
-fig, ax = plt.subplots(4, 2, figsize=(10, 10), sharex=True)
-fig.suptitle('Joint Positions')
-for i in range(8):
-    col = 0 if i < 4 else 1
-    row = i if i < 4 else i - 4
-    ax[row, col].plot(dfs_state[i]['timestamp'], dfs_state[i]['pos'], label='pos_' + list_joints[i])
-    ax[row, col].plot(dfs_traj[i]['timestamp'], dfs_traj[i]['pos_des'], label='pos_des_' + list_joints[i])
-    ax[row, col].legend()
-plt.tight_layout()
+    for joint_name in list_joints:
+        df_joint_state = pd.read_csv(rosbag_path + f'/{joint_name}_state.csv')
+        df_joint_traj = pd.read_csv(rosbag_path + f'/{joint_name}_traj.csv')
+        # df_joint_state['timestamp'] = df_joint_state['timestamp'] - df_joint_state['timestamp'][0]
+        # df_joint_traj['timestamp'] = df_joint_traj['timestamp'] - df_joint_traj['timestamp'][0]
 
-# Plot the joint velocities.
-fig, ax = plt.subplots(4, 2, figsize=(10, 10), sharex=True)
-fig.suptitle('Joint Velocities')
-for i in range(8):
-    col = 0 if i < 4 else 1
-    row = i if i < 4 else i - 4
-    ax[row, col].plot(dfs_state[i]['timestamp'], dfs_state[i]['vel'], label='vel_' + list_joints[i])
-    ax[row, col].plot(dfs_traj[i]['timestamp'], dfs_traj[i]['vel_des'], label='vel_des_' + list_joints[i])
-    ax[row, col].legend()
-plt.tight_layout()
+        if DES_TIME_CUT is not None:
+            df_joint_state = df_joint_state[(df_joint_state['timestamp'] >= DES_TIME_CUT[0]) & (df_joint_state['timestamp'] <= DES_TIME_CUT[1])]
+            df_joint_traj = df_joint_traj[(df_joint_traj['timestamp'] >= DES_TIME_CUT[0]) & (df_joint_traj['timestamp'] <= DES_TIME_CUT[1])]
+        dfs_joint_state.append(df_joint_state)
+        dfs_joint_traj.append(df_joint_traj)
 
-# Plot the joint efforts.
-fig, ax = plt.subplots(4, 2, figsize=(10, 10), sharex=True)
-fig.suptitle('Joint Effort')
-for i in range(8):
-    col = 0 if i < 4 else 1
-    row = i if i < 4 else i - 4
-    ax[row, col].plot(dfs_state[i]['timestamp'], dfs_state[i]['eff'], label='eff_' + list_joints[i])
-    ax[row, col].plot(dfs_traj[i]['timestamp'], dfs_traj[i]['eff_des'], label='eff_des_' + list_joints[i])
-    ax[row, col].legend()
-plt.tight_layout()
 
-plt.show()
+    df_dcm_desired = pd.read_csv(rosbag_path + '/dcm_des.csv')
+    df_dcm_desired['timestamp'] = df_dcm_desired['timestamp'] - df_dcm_desired['timestamp'][0]
+    df_dcm = pd.read_csv(rosbag_path + '/dcm.csv')
+    df_dcm['timestamp'] = df_dcm['timestamp'] - df_dcm['timestamp'][0]
+    df_dcm_next = pd.read_csv(rosbag_path + '/dcm_next.csv')
+    df_dcm_next['timestamp'] = df_dcm_next['timestamp'] - df_dcm_next['timestamp'][0]
+    df_dcm_predicted = pd.read_csv(rosbag_path + '/dcm_predicted.csv')
+    df_dcm_predicted['timestamp'] = df_dcm_predicted['timestamp'] - df_dcm_predicted['timestamp'][0]
+
+    # Plot the joint positions.
+    fig, ax = plt.subplots(4, 2, figsize=(10, 10), sharex=True)
+    fig.suptitle('Joint Positions' + rosbag_path)
+    for i in range(8):
+        col = 0 if i < 4 else 1
+        row = i if i < 4 else i - 4
+        ax[row, col].plot(dfs_joint_state[i]['timestamp'], dfs_joint_state[i]['pos'], label='pos_' + list_joints[i])
+        ax[row, col].plot(dfs_joint_traj[i]['timestamp'], dfs_joint_traj[i]['pos_des'], label='pos_des_' + list_joints[i])
+        ax[row, col].legend()
+    plt.tight_layout()
+
+    # Plot the joint velocities.
+    fig, ax = plt.subplots(4, 2, figsize=(10, 10), sharex=True)
+    fig.suptitle('Joint Velocities ' + rosbag_path)
+    for i in range(8):
+        col = 0 if i < 4 else 1
+        row = i if i < 4 else i - 4
+        ax[row, col].plot(dfs_joint_state[i]['timestamp'], dfs_joint_state[i]['vel'], label='vel_' + list_joints[i])
+        ax[row, col].plot(dfs_joint_traj[i]['timestamp'], dfs_joint_traj[i]['vel_des'], label='vel_des_' + list_joints[i])
+        ax[row, col].legend()
+    plt.tight_layout()
+
+    # Plot the joint efforts.
+    fig, ax = plt.subplots(4, 2, figsize=(10, 10), sharex=True)
+    fig.suptitle('Joint Effort' + rosbag_path)
+    for i in range(8):
+        col = 0 if i < 4 else 1
+        row = i if i < 4 else i - 4
+        ax[row, col].plot(dfs_joint_state[i]['timestamp'], dfs_joint_state[i]['eff'], label='eff_' + list_joints[i])
+        ax[row, col].plot(dfs_joint_traj[i]['timestamp'], dfs_joint_traj[i]['eff_des'], label='eff_des_' + list_joints[i])
+        ax[row, col].legend()
+    plt.tight_layout()
+
+    # Plot capture point data
+    print(df_dcm_desired.head())
+    fig, ax = plt.subplots(1, 1, figsize=(10, 10))
+    fig.suptitle('Capture Point Data' + rosbag_path)
+    # ax.plot(df_dcm_next['timestamp'], df_dcm_next['next_footstep_x'], '*', label='next_footstep')
+    ax.plot(df_dcm_desired['timestamp'], df_dcm_desired['dcm_x'], 'go', label='desired_dcm')
+    ax.plot(df_dcm['timestamp'], df_dcm['dcm_x'], 'r*', label='dcm')
+
+
+if args.plot_data:
+
+    # rosbag_path_old = '/home/sorina/Documents/code/biped_hardware/bags/20241013-11-33-35.bag'
+    # plot_data(rosbag_path_old)
+
+    plot_data(rosbag_path)
+
+    plt.show()
 
 
 
