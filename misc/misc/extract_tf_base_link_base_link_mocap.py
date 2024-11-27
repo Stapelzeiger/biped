91ec8945c63afadf16a8abb4b82fae8b3a615cba
Sorina <lupusorina@yahoo.com>
2024-11-26 19:34:51 -0800

    fixed for the ransac

--- misc/misc/extract_tf_base_link_base_link_mocap.py
+++ misc/misc/extract_tf_base_link_base_link_mocap.py
@@ -7,8 +7,22 @@ from motion_capture_tracking_interfaces.msg import NamedPoseArray
 import numpy as np
 import threading
 from scipy.spatial.transform import Rotation as R
+import pandas as pd
 
-class TransformLookupNode(Node):
+def apply_rotation_to_vector(q):
+    '''
+    Apply a quaternion rotation to a unit vector [1, 0, 0].
+
+    q: quaternion [x, y, z, w]
+    Returns: Rotated vector as a numpy array.
+    '''
+    print(q)
+    v = np.array([1, 0, 0])
+    rot = R.from_quat([q.x, q.y, q.z, q.w])
+    rotated_v = rot.apply(v)
+    return rotated_v
+
+class RansacNode(Node):
     def __init__(self):
         super().__init__('transform_lookup_node')
 
@@ -16,7 +30,7 @@ class TransformLookupNode(Node):
         self.tf_buffer = Buffer()
         self.tf_listener = TransformListener(self.tf_buffer, self)
 
-        # params.
+        # Params.
         self.declare_parameters(
             namespace='',
             parameters=[
@@ -25,7 +39,10 @@ class TransformLookupNode(Node):
                 ('l_foot_frame', 'L_FOOT_CONTACT'),
                 ('world_frame', 'world'),
                 ('pose_base_link_mocap_name', 'biped'),
+                ('pose_right_foot_mocap_name', 'right_foot'),
+                ('pose_left_foot_mocap_name', 'left_foot'),
                 ('dt', 0.01),
+                ('save_to_csv', True)
             ]
         )
         self.params = {
@@ -34,10 +51,15 @@ class TransformLookupNode(Node):
             'l_foot_frame': self.get_parameter('l_foot_frame').get_parameter_value().string_value,
             'world_frame': self.get_parameter('world_frame').get_parameter_value().string_value,
             'pose_base_link_mocap_name': self.get_parameter('pose_base_link_mocap_name').get_parameter_value().string_value,
+            'pose_right_foot_mocap_name': self.get_parameter('pose_right_foot_mocap_name').get_parameter_value().string_value,
+            'pose_left_foot_mocap_name': self.get_parameter('pose_left_foot_mocap_name').get_parameter_value().string_value,
             'dt': self.get_parameter('dt').get_parameter_value().double_value,
+            'save_to_csv': self.get_parameter('save_to_csv').get_parameter_value().bool_value
         }
         self.frames = [self.params['base_link_frame'], self.params['r_foot_frame'], self.params['l_foot_frame']]
         self.get_logger().info(f"Parameters: {self.params}")
+
+        self.list_refined_translation = []
 
         self.lock = threading.Lock()
 
@@ -63,41 +85,76 @@ class TransformLookupNode(Node):
             if not any([pose.name == self.params['pose_base_link_mocap_name'] for pose in msg.poses]):
                 self.get_logger().warn(f"Could not find pose with name {self.params['pose_base_link_mocap_name']}")
                 return
+            if not any([pose.name == self.params['pose_right_foot_mocap_name'] for pose in msg.poses]):
+                self.get_logger().warn(f"Could not find pose with name {self.params['pose_right_foot_mocap_name']}")
+                return
+            if not any([pose.name == self.params['pose_left_foot_mocap_name'] for pose in msg.poses]):
+                self.get_logger().warn(f"Could not find pose with name {self.params['pose_left_foot_mocap_name']}")
+                return
 
-            # Get the pose of the base_link_mocap.
+            # Get the pose from mocap.
             self.poses_vicon_msg = msg
             for pose in self.poses_vicon_msg.poses:
                 if pose.name == self.params['pose_base_link_mocap_name']:
                    pose_base_link_mocap = pose
+                elif pose.name == self.params['pose_right_foot_mocap_name']:
+                    pose_right_foot_mocap = pose
+                elif pose.name == self.params['pose_left_foot_mocap_name']:
+                    pose_left_foot_mocap = pose
 
             # Compute the rotation vector. For rotation, use a unit vector in the x-direction.
-            v = np.array([1, 0, 0])
-            q_base_link_mocap = [pose_base_link_mocap.pose.orientation.x,
-                                            pose_base_link_mocap.pose.orientation.y,
-                                            pose_base_link_mocap.pose.orientation.z,
-                                            pose_base_link_mocap.pose.orientation.w]
-            rot = R.from_quat(q_base_link_mocap)
-            rotated_v_motion_capture = rot.apply(v)
-            self.points_base_link_mocap.append(np.array([pose_base_link_mocap.pose.position.x,
-                                                         pose_base_link_mocap.pose.position.y,
-                                                         pose_base_link_mocap.pose.position.z,
-                                                         rotated_v_motion_capture[0],
-                                                         rotated_v_motion_capture[1],
-                                                         rotated_v_motion_capture[2]]))
+            rotated_v_base_link_mocap = apply_rotation_to_vector(pose_base_link_mocap.pose.orientation)
+            rotated_v_right_foot_mocap = apply_rotation_to_vector(pose_right_foot_mocap.pose.orientation)
+            rotated_v_left_foot_mocap = apply_rotation_to_vector(pose_left_foot_mocap.pose.orientation)
             
-            # Get the base_link transform TF.
+            row_mocap = [pose_base_link_mocap.pose.position.x,
+                        pose_base_link_mocap.pose.position.y,
+                        pose_base_link_mocap.pose.position.z,
+                        rotated_v_base_link_mocap[0],
+                        rotated_v_base_link_mocap[1],
+                        rotated_v_base_link_mocap[2],
+                        pose_right_foot_mocap.pose.position.x,
+                        pose_right_foot_mocap.pose.position.y,
+                        pose_right_foot_mocap.pose.position.z,
+                        rotated_v_right_foot_mocap[0],
+                        rotated_v_right_foot_mocap[1],
+                        rotated_v_right_foot_mocap[2],
+                        pose_left_foot_mocap.pose.position.x,
+                        pose_left_foot_mocap.pose.position.y,
+                        pose_left_foot_mocap.pose.position.z,
+                        rotated_v_left_foot_mocap[0],
+                        rotated_v_left_foot_mocap[1],
+                        rotated_v_left_foot_mocap[2]]
+            self.points_base_link_mocap.append(np.array(row_mocap))
+
+            # Get the TFs.
             base_link_trans, base_link_rot = self.lookup_transform(self.params['world_frame'], self.params['base_link_frame'])
+            right_foot_trans, right_foot_rot = self.lookup_transform(self.params['world_frame'], self.params['r_foot_frame'])
+            left_foot_trans, left_foot_rot = self.lookup_transform(self.params['world_frame'], self.params['l_foot_frame'])
 
-            q_base_link = [base_link_rot.x, base_link_rot.y, base_link_rot.z, base_link_rot.w]
-            rot = R.from_quat(q_base_link)
-            rotated_v_base_link = rot.apply(v)
+            rotated_v_base_link = apply_rotation_to_vector(base_link_rot)
+            rotated_v_right_foot = apply_rotation_to_vector(right_foot_rot)
+            rotated_v_left_foot = apply_rotation_to_vector(left_foot_rot)
 
-            self.points_base_link.append(np.array([base_link_trans.x,
-                                                    base_link_trans.y,
-                                                    base_link_trans.z, 
-                                                    rotated_v_base_link[0],
-                                                    rotated_v_base_link[1],
-                                                    rotated_v_base_link[2]]))
+            row_tfs = [base_link_trans.x,
+                        base_link_trans.y,
+                        base_link_trans.z,
+                        rotated_v_base_link[0],
+                        rotated_v_base_link[1],
+                        rotated_v_base_link[2],
+                        right_foot_trans.x,
+                        right_foot_trans.y,
+                        right_foot_trans.z,
+                        rotated_v_right_foot[0],
+                        rotated_v_right_foot[1],
+                        rotated_v_right_foot[2],
+                        left_foot_trans.x,
+                        left_foot_trans.y,
+                        left_foot_trans.z,
+                        rotated_v_left_foot[0],
+                        rotated_v_left_foot[1],
+                        rotated_v_left_foot[2]]
+            self.points_base_link.append(np.array(row_tfs))
 
     def lookup_transform(self, target_frame: str, source_frame: str):
         try:
@@ -106,17 +163,15 @@ class TransformLookupNode(Node):
                 source_frame,
                 rclpy.time.Time()
             )
-
             translation = transform.transform.translation
             rotation = transform.transform.rotation
-
             return translation, rotation
 
         except Exception as e:
             self.get_logger().warn(f"Could not get transform: {e}")
             return
 
-    def ransac_translation(self, points_model, points_vicon, threshold=0.01, max_iterations=100):
+    def ransac_translation(self, points_model, points_vicon, threshold=0.1, max_iterations=100):
         best_inliers = []
         best_translation = None
         N = 100
@@ -161,17 +216,20 @@ class TransformLookupNode(Node):
                 np.array(self.points_base_link),
                 np.array(self.points_base_link_mocap)
             )
-            pos = refined_translation[0:3]
-            rot = refined_translation[3:]
-            # Get rotation matrix.
-            rot = R.from_rotvec(rot)
-            self.get_logger().info(f"RANSAC translation: {refined_translation[0:3]}, rotation {rot.as_quat()}")
+            # self.get_logger().info(f"Refined translation: {refined_translation}")
+
+            if self.params['save_to_csv']:
+                # Save to csv.
+                # self.get_logger().info("Saving to CSV")
+                self.list_refined_translation.append(refined_translation)
+                df = pd.DataFrame(self.list_refined_translation)
+                df.to_csv('/home/sorina/Documents/code/biped_hardware/ros2_ws/src/biped/misc/misc/ransac_data/refined_translation.csv', index=False)
         except ValueError as e:
             self.get_logger().warn(f"RANSAC failed: {e}")
 
 def main(args=None):
     rclpy.init(args=args)
-    node = TransformLookupNode()
+    node = RansacNode()
     rclpy.spin(node)
     node.destroy_node()
     rclpy.shutdown()
